765888bfb78d313e2653c6608fbdfcb7a067a941
jpuccia <joseph.puccia@pds.nl>
2016-02-22 05:53:50 -0600

    Moved BootstrapperHarness to its own class file.

---
+++ src/Witsml.Studio.UnitTest/BootstrapperHarness.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PDS.Witsml.Studio
+{
+    public class BootstrapperHarness : Bootstrapper
+    {
+        public BootstrapperHarness() : base(false)
+        {
+        }
+
+        public IEnumerable<Assembly> CallSelectAssemblies()
+        {
+            //this.OnStartup()
+
+            return SelectAssemblies();
+        }
+
+        public void CallOnStartup()
+        {
+            //SelectAssemblies();
+            //this.StartDesignTime();
+            //this.StartRuntime();
+            //var windowsManager = this.GetInstance(typeof(IWindowManager), string.Empty);
+            //var shellVm = this.GetInstance(typeof(IShellViewModel), string.Empty);
+
+            //var shellVm = new ShellViewModel();
+            OnStartup(null, null);
+        }
+
+        //public T GetInstance<T>()
+        //{
+        //    try
+        //    {
+        //        return (T)base.GetInstance(typeof(T), string.Empty);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //this.WhatDoIHave();
+        //        throw;
+        //    }
+        //}
+
+        //protected override void StartRuntime()
+        //{
+        //    this.Configure();
+        //    //this.SelectAssemblies();
+        //}
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return base.SelectAssemblies()
+                .Union(new[] { GetType().Assembly });
+        }
+    }
+}
--- src/Witsml.Studio.UnitTest/BootstrapperTests.cs
+++ src/Witsml.Studio.UnitTest/BootstrapperTests.cs
@@ -15,57 +15,6 @@ namespace PDS.Witsml.Studio
     {
         private BootstrapperHarness bootstrapper;
 
-        private class BootstrapperHarness : Bootstrapper
-        {
-            public BootstrapperHarness():base(false)
-            {
-            }
-
-            public IEnumerable<Assembly> CallSelectAssemblies()
-            {
-                //this.OnStartup()
-
-                return SelectAssemblies();
-            }
-
-            public void CallOnStartup()
-            {
-                //SelectAssemblies();
-                //this.StartDesignTime();
-                //this.StartRuntime();
-                //var windowsManager = this.GetInstance(typeof(IWindowManager), string.Empty);
-                //var shellVm = this.GetInstance(typeof(IShellViewModel), string.Empty);
-
-                //var shellVm = new ShellViewModel();
-                OnStartup(null, null);
-            }
-
-            //public T GetInstance<T>()
-            //{
-            //    try
-            //    {
-            //        return (T)base.GetInstance(typeof(T), string.Empty);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        //this.WhatDoIHave();
-            //        throw;
-            //    }
-            //}
-
-            //protected override void StartRuntime()
-            //{
-            //    this.Configure();
-            //    //this.SelectAssemblies();
-            //}
-
-            protected override IEnumerable<Assembly> SelectAssemblies()
-            {
-                return base.SelectAssemblies()
-                    .Union(new[] { GetType().Assembly });
-            }
-        }
-
         [TestInitialize]
         public void TestSetUp()
         {
