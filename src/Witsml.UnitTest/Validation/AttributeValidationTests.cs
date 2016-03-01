022ef35e38956abc83e17f77a48fb3df903af5e2
Haifeng Chen <haifeng.chen@pds.nl>
2016-03-01 10:34:15 -0600

    Create custom validation attributes for nested property and collection property

---
+++ src/Witsml.UnitTest/Validation/AttributeValidationTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDS.Framework;
+
+namespace PDS.Witsml.Validation
+{
+    /// <summary>
+    /// Testing class for custom validation attributes
+    /// </summary>
+    [TestClass]
+    public class AttributeValidationTests
+    {
+        /// <summary>
+        /// Test <see cref="ObjectAttribute"/> validation attribute for nested properties
+        /// </summary>
+        [TestMethod]
+        public void Test_Object_Validation_Attributes()
+        {
+            var dtoWell = new DtoWell
+            {
+                Name = "Well-1",
+                Elevation = new Elevation(),
+                WellDatum = new List<WellDatum> { new WellDatum { Name = "Sea Level", Code = "SL", Uid = "SL" } }
+            };
+
+            IList<ValidationResult> results;
+            var valid = EntityValidator.TryValidate(dtoWell, out results);
+
+            Assert.IsFalse(valid);
+        }
+
+        /// <summary>
+        /// Test <see cref="CollectionAttribute"/> validation attribute for collection property
+        /// </summary>
+        [TestMethod]
+        public void Test_Collection_Validation_Attributes()
+        {
+            var dtoWell = new DtoWell
+            {
+                Name = "Well-1",
+                Elevation = new Elevation { Uom = "m" },
+                WellDatum = new List<WellDatum> { new WellDatum { Name = "Sea Level", Code = "SL" } }
+            };
+
+            IList<ValidationResult> results;
+            var valid = EntityValidator.TryValidate(dtoWell, out results);
+
+            Assert.IsFalse(valid);
+        }
+    }
+}
---
+++ src/Witsml.UnitTest/Validation/CustomValidationAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using PDS.Framework;
+
+namespace PDS.Witsml.Validation
+{
+    /// <summary>
+    /// Custom validation attribute that specifies how a non-primitive property is validated
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.DataAnnotations.ValidationAttribute" />
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class ObjectAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IList<ValidationResult> results;
+            EntityValidator.TryValidate(value, out results);
+            return results.FirstOrDefault();
+        }
+    }
+
+
+    /// <summary>
+    /// Custom validation attribute that specifies how a collection property is validated
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.DataAnnotations.ValidationAttribute" />
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class CollectionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var list = (IEnumerable)value;
+            foreach (var obj in list)
+            {
+                IList<ValidationResult> results;
+                EntityValidator.TryValidate(obj, out results);
+                if (results.Count > 0)
+                    return results.FirstOrDefault();
+            }
+            return null;
+        }
+    }
+}
