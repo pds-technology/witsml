9d7fb713263ffbbf2e6b8af07fcc9cf48cbd623a
Haifeng Chen <haifeng.chen@pds.nl>
2016-03-01 14:18:27 -0600

    Remove dependency on PDS.Framework.EntityValidator class
    Related Work Items: #4671, #4673

--- src/Witsml.UnitTest/Validation/CustomValidationAttributes.cs
+++ src/Witsml.UnitTest/Validation/CustomValidationAttributes.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using PDS.Framework;
 
 namespace PDS.Witsml.Validation
 {
@@ -48,6 +47,26 @@ namespace PDS.Witsml.Validation
                 uids.Add(uid);
             }
             return null;
+        }
+    }
+
+    /// <summary>
+    /// Defines a helper method that can be used to validate objects using data annotation attributes.
+    /// </summary>
+    public static class EntityValidator
+    {
+        /// <summary>
+        /// Determines whether the specified object is valid.
+        /// </summary>
+        /// <param name="instance">The object instance.</param>
+        /// <param name="results">The validation results.</param>
+        /// <returns>true if the object is valid; otherwise, false</returns>
+        public static bool TryValidate(object instance, out IList<ValidationResult> results)
+        {
+            var context = new ValidationContext(instance, serviceProvider: null, items: null);
+            results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(instance, context, results, validateAllProperties: true);
         }
     }
 }
