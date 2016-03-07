fa83e30f794d93c6efe71cd3b506a68cd94e9985
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-07 08:51:42 -0600

    Added new classes as starting point for Channel Streaming implementation.
    Related Work Items: #4768

---
+++ src/Witsml.Server/Providers/ChannelStreaming/ChannelStreamingProducer.cs
@@ -0,0 +1,8 @@
+﻿using Energistics.Protocol.ChannelStreaming;
+
+namespace PDS.Witsml.Server.Providers.ChannelStreaming
+{
+    public class ChannelStreamingProducer : ChannelStreamingProducerHandler
+    {
+    }
+}
