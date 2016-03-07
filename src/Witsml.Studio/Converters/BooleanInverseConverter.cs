eebd6021a7611d21c7904687f5ffa02ec3488519
jpuccia <joseph.puccia@pds.nl>
2016-03-07 15:57:10 -0600

    Added a BooleanInverseConverter and used that to set IsEnabled for the Paste context menu to the inverse of its IsReadOnly property instead of using a separate property.
    Related Work Items: #4772

---
+++ src/Witsml.Studio/Converters/BooleanInverseConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PDS.Witsml.Studio.Converters
+{
+    public class BooleanInverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = false;
+            if ((value == null ? true : !bool.TryParse(value.ToString(), out flag)))
+            {
+                return Binding.DoNothing;
+            }
+            return !flag;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
