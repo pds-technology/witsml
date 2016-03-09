c1de0e67dfc8340227e967971f299b9c678a6d8d
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-08 20:16:25 -0600

    Updated Discovery and Store protocol providers to add support for WITSML 2.0 data objects.
    Related Work Items: #4551

---
+++ src/Witsml.Server.MongoDb/Data/Channels/ChannelSet200DataAdapter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Energistics.DataAccess.WITSML200;
+using Energistics.Datatypes;
+
+namespace PDS.Witsml.Server.Data.Channels
+{
+    /// <summary>
+    /// Data adapter that encapsulates CRUD functionality for <see cref="ChannelSet" />
+    /// </summary>
+    /// <seealso cref="PDS.Witsml.Server.Data.MongoDbDataAdapter{Energistics.DataAccess.WITSML200.ChannelSet}" />
+    [Export(typeof(IEtpDataAdapter<ChannelSet>))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class ChannelSet200DataAdapter : MongoDbDataAdapter<ChannelSet>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChannelSet200DataAdapter"/> class.
+        /// </summary>
+        /// <param name="databaseProvider">The database provider.</param>
+        [ImportingConstructor]
+        public ChannelSet200DataAdapter(IDatabaseProvider databaseProvider) : base(databaseProvider, ObjectNames.ChannelSet200, ObjectTypes.Uuid)
+        {
+        }
+
+        /// <summary>
+        /// Gets a collection of data objects related to the specified URI.
+        /// </summary>
+        /// <param name="parentUri">The parent URI.</param>
+        /// <returns>A collection of data objects.</returns>
+        public override List<ChannelSet> GetAll(EtpUri? parentUri = null)
+        {
+            var query = GetQuery().AsQueryable();
+
+            //if (parentUri != null)
+            //{
+            //    var uidLog = parentUri.Value.ObjectId;
+            //    query = query.Where(x => x.Log.Uuid == uidLog);
+            //}
+
+            return query
+                .OrderBy(x => x.Citation.Title)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Puts the specified data object into the data store.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public override WitsmlResult Put(ChannelSet entity)
+        {
+            if (!string.IsNullOrWhiteSpace(entity.Uuid) && Exists(entity.Uuid))
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                entity.Uuid = NewUid(entity.Uuid);
+                entity.Citation = entity.Citation.Update();
+
+                var validator = Container.Resolve<IDataObjectValidator<ChannelSet>>();
+                validator.Validate(Functions.PutObject, entity);
+
+                InsertEntity(entity);
+            }
+
+            return new WitsmlResult(ErrorCodes.Success, entity.Uuid);
+        }
+    }
+}
