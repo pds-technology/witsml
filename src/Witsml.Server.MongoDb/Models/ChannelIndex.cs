ec400a055d93d655f02da069537baf2e2017827a
Haifeng Chen <haifeng.chen@pds.nl>
2016-03-16 11:44:46 -0500

    Create new data model class for log data and register with MongoDb
    Related Work Items: #4860

---
+++ src/Witsml.Server.MongoDb/Models/ChannelIndex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PDS.Witsml.Server.Models
+{
+    [Serializable]
+    public class ChannelIndex
+    {
+        public string Mnemonic { get; set; }
+
+        public bool Increasing { get; set; }
+
+        public double Start { get; set; }
+
+        public double End { get; set; }
+    }
+}
