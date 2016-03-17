2dc5f22c32bb0ffb5cd66dcea04f4894caeb6fd6
Haifeng Chen <haifeng.chen@pds.nl>
2016-03-17 09:53:53 -0500

    Refactor ChannelDataAdapter
    Related Work Items: #4906

--- src/Witsml.Server.MongoDb/Data/Logs/ChannelDataAdapter.cs
+++ src/Witsml.Server.MongoDb/Data/Logs/ChannelDataAdapter.cs
@@ -1,19 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using PDS.Witsml.Server.Models;
 using PDS.Witsml.Server.Properties;
 
 namespace PDS.Witsml.Server.Data.Logs
 {
-    public class ChannelDataAdapter
+    public class ChannelDataAdapter : MongoDbDataAdapter<ChannelSetValues>
     {
         private static readonly int RangeSize = Settings.Default.LogIndexRangeSize;
 
-        public List<ChannelSetValues> CreateChannelSetValuesList(string data, string uidLog, string uidChannelSet, List<ChannelIndexInfo> indices)
+        [ImportingConstructor]
+        public ChannelDataAdapter(IDatabaseProvider databaseProvider) : base(databaseProvider, ObjectTypes.ChannelSetValues, ObjectTypes.Uid)
+        {
+            
+        }
+
+        public void WriteChannelSetValues(string uidLog, Dictionary<string, string> channelData, Dictionary<string, List<ChannelIndexInfo>> indicesMap)
+        {
+            var collection = GetCollection<ChannelSetValues>(DbCollectionName);
+            var dataChunks = new List<ChannelSetValues>();
+            foreach (var key in indicesMap.Keys)
+            {
+                var dataChunk = CreateChannelSetValuesList(channelData[key], uidLog, key, indicesMap[key]);
+                if (dataChunk != null && dataChunk.Count > 0)
+                    dataChunks.AddRange(dataChunk);
+            }
+
+            collection.BulkWrite(dataChunks
+                .Select(dc =>
+                {
+                    return (WriteModel<ChannelSetValues>)new InsertOneModel<ChannelSetValues>(dc);
+                }));
+        }
+
+        public List<ChannelSetValues> ParseLogData(string data, string uidLog)
+        {
+            var logData = DeserializeLogData(data);
+            return null;
+        }
+
+        public List<List<List<object>>> DeserializeChannelSetData(string data)
+        {
+            return JsonConvert.DeserializeObject<List<List<List<object>>>>(data);
+        }
+
+        public string SerializeChannelSetData(List<List<List<object>>> data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public List<List<object>> DeserializeLogData(string data)
+        {
+            return JsonConvert.DeserializeObject<List<List<object>>>(data);
+        }
+
+        public string SerializeLogData(List<List<object>> data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public Tuple<int, int> ComputeRange(double index, int rangeSize, bool increasing = true)
+        {
+            var rangeIndex = increasing ? (int)(Math.Floor(index / rangeSize)) : (int)(Math.Ceiling(index / rangeSize));
+            return new Tuple<int, int>(rangeIndex * rangeSize, rangeIndex * rangeSize + (increasing ? rangeSize : -rangeSize));
+        }
+
+        private List<ChannelSetValues> CreateChannelSetValuesList(string data, string uidLog, string uidChannelSet, List<ChannelIndexInfo> indices)
         {
             var dataChunks = new List<ChannelSetValues>();
             var logData = DeserializeChannelSetData(data);
@@ -87,50 +143,8 @@ namespace PDS.Witsml.Server.Data.Logs
                 }
                 while (rangeSize.Item2 >= end);
             }
-            
+
             return dataChunks;
-        }
-
-        public List<ChannelSetValues> ParseLogData(string data, string uidLog)
-        {
-            var logData = DeserializeLogData(data);
-            return null;
-        }
-
-        public List<List<List<object>>> DeserializeChannelSetData(string data)
-        {
-            return JsonConvert.DeserializeObject<List<List<List<object>>>>(data);
-        }
-
-        public string SerializeChannelSetData(List<List<List<object>>> data)
-        {
-            return JsonConvert.SerializeObject(data);
-        }
-
-        public List<List<object>> DeserializeLogData(string data)
-        {
-            return JsonConvert.DeserializeObject<List<List<object>>>(data);
-        }
-
-        public string SerializeLogData(List<List<object>> data)
-        {
-            return JsonConvert.SerializeObject(data);
-        }
-
-        public Tuple<int, int> ComputeRange(double index, int rangeSize, bool increasing = true)
-        {
-            var rangeIndex = increasing ? (int)(Math.Floor(index / rangeSize)) : (int)(Math.Ceiling(index / rangeSize));
-            return new Tuple<int, int>(rangeIndex * rangeSize, rangeIndex * rangeSize + (increasing ? rangeSize : -rangeSize));
-        }
-
-        private string NewUid(string uid = null)
-        {
-            if (string.IsNullOrEmpty(uid))
-            {
-                uid = Guid.NewGuid().ToString();
-            }
-
-            return uid;
         }
 
         private void SetChunkIndices(List<object> starts, List<object> ends, List<ChannelIndexInfo> indices)
