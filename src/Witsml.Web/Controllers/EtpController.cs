868a2a85bdfa0efe1e7883bce5b9ad5c36af9bc5
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-22 19:40:53 -0500

    Added Server Capabilities action method to the ETP Web Api controller.
    Related Work Items: #5008

--- src/Witsml.Web/Controllers/EtpController.cs
+++ src/Witsml.Web/Controllers/EtpController.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Net;
 using System.Net.Http;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Description;
 using System.Web.WebSockets;
 using Energistics;
+using Energistics.Datatypes;
 using Energistics.Protocol.ChannelStreaming;
 using Energistics.Protocol.Discovery;
 using Energistics.Protocol.Store;
 using PDS.Framework;
-using PDS.Witsml.Web.Properties;
+using PDS.Witsml.Server.Properties;
 
 namespace PDS.Witsml.Web.Controllers
 {
@@ -22,8 +26,8 @@ namespace PDS.Witsml.Web.Controllers
     [PartCreationPolicy(CreationPolicy.NonShared)]
     public class EtpController : ApiController
     {
-        private static readonly string EtpSocketServerName = Settings.Default.EtpSocketServerName;
-        private static readonly string EtpSocketServerVersion = Settings.Default.EtpSocketServerVersion;
+        private static readonly string DefaultServerName = Settings.Default.DefaultServerName;
+        private static readonly string DefaultServerVersion = Settings.Default.DefaultServerVersion;
         private readonly IContainer _container;
 
         [ImportingConstructor]
@@ -52,16 +56,47 @@ namespace PDS.Witsml.Web.Controllers
                 new { error = "Invalid web socket request" });
         }
 
+        // GET: api/etp/ServerCapabilities
+        [Route("api/etp/ServerCapabilities")]
+        [ResponseType(typeof(ServerCapabilities))]
+        public IHttpActionResult GetServerCapabilities()
+        {
+            var handler = CreateEtpServerHandler(null);
+
+            var capServer = new ServerCapabilities()
+            {
+                ApplicationName = handler.ApplicationName,
+                ApplicationVersion = handler.ApplicationVersion,
+                SupportedProtocols = handler.GetSupportedProtocols(),
+                SessionId = Guid.NewGuid().ToString(),
+                SupportedObjects = new string[0],
+                ContactInfomration = new Contact()
+                {
+                    OrganizationName = Settings.Default.DefaultVendorName,
+                    ContactName = Settings.Default.DefaultContactName,
+                    ContactEmail = Settings.Default.DefaultContactEmail,
+                    ContactPhone = Settings.Default.DefaultContactPhone
+                }
+            };
+
+            return Ok(capServer);
+        }
+
         private async Task AcceptWebSocketRequest(AspNetWebSocketContext context)
         {
-            var socket = context.WebSocket as AspNetWebSocket;
-            var handler = new EtpServerHandler(socket, EtpSocketServerName, EtpSocketServerVersion);
+            var handler = CreateEtpServerHandler(context.WebSocket);
+            await handler.Accept(context);
+        }
+
+        private EtpServerHandler CreateEtpServerHandler(WebSocket socket)
+        {
+            var handler = new EtpServerHandler(socket, DefaultServerName, DefaultServerVersion);
 
             handler.Register(() => _container.Resolve<IChannelStreamingProducer>());
             handler.Register(() => _container.Resolve<IDiscoveryStore>());
             handler.Register(() => _container.Resolve<IStoreStore>());
 
-            await handler.Accept(context);
+            return handler;
         }
     }
 }
