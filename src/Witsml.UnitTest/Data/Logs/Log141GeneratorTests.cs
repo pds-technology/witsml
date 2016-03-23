419d0d87ebca903a17cefe8fb30707219d0d3d74
Cristina Ip <cristina.ip@pds.nl>
2016-03-22 19:31:43 -0500

    Implemented Log 141 generator class.
    Related Work Items: #5007

---
+++ src/Witsml.UnitTest/Data/Logs/Log141GeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Energistics.DataAccess.WITSML141;
+using Energistics.DataAccess.WITSML141.ComponentSchemas;
+using Energistics.DataAccess.WITSML141.ReferenceData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDS.Witsml.Data.Logs
+{
+    [TestClass]
+    public class Log141GeneratorTests
+    {
+        private Log141Generator LogGenerator;
+        private Log DepthLog;
+        private Log TimeLog;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            LogGenerator = new Log141Generator();
+            DepthLog = new Log() { IndexType = LogIndexType.measureddepth };
+            TimeLog = new Log() { IndexType = LogIndexType.datetime };
+        }
+
+        [TestMethod]
+        public void Can_generate_depth_log()
+        {
+            Log log = LogGenerator.CreateLog(DepthLog, LogIndexDirection.increasing, 10);
+            Assert.IsNotNull(log);
+            Assert.IsNotNull(log.LogData);
+            Assert.IsNotNull(log.LogData[0].Data);
+            Assert.AreEqual(10, log.LogData[0].Data.Count);
+        }
+
+        [TestMethod]
+        public void Can_generate_time_log()
+        {
+            Log log = LogGenerator.CreateLog(TimeLog, LogIndexDirection.increasing, 10);
+            Assert.IsNotNull(log);
+            Assert.IsNotNull(log.LogData);
+            Assert.IsNotNull(log.LogData[0].Data);
+            Assert.AreEqual(10, log.LogData[0].Data.Count);
+        }
+    }
+}
---
+++ src/Witsml/Data/Logs/Log141Generator.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Energistics.DataAccess.WITSML141;
+using Energistics.DataAccess.WITSML141.ComponentSchemas;
+using Energistics.DataAccess.WITSML141.ReferenceData;
+using PDS.Framework;
+
+namespace PDS.Witsml.Data.Logs
+{
+    public class Log141Generator : DataGenerator
+    {
+        public readonly LogIndexType[] DepthIndexTypes = new LogIndexType[] { LogIndexType.length, LogIndexType.measureddepth, LogIndexType.verticaldepth };
+        public readonly LogIndexType[] TimeIndexTypes = new LogIndexType[] { LogIndexType.datetime, LogIndexType.elapsedtime };
+        public readonly LogIndexType[] OtherIndexTypes = new LogIndexType[] { LogIndexType.other, LogIndexType.unknown };
+
+        public LogCurveInfo CreateDateTimeLogCurveInfo(string name, string unit)
+        {
+            return CreateLogCurveInfo(name, unit, LogDataType.datetime);
+        }
+
+        public LogCurveInfo CreateDoubleLogCurveInfo(string name, string unit)
+        {
+            return CreateLogCurveInfo(name, unit, LogDataType.@double);
+        }
+
+        public LogCurveInfo CreateStringLogCurveInfo(string name, string unit)
+        {
+            return CreateLogCurveInfo(name, unit, LogDataType.@string);
+        }
+
+        public LogCurveInfo CreateLogCurveInfo(string name, string unit, LogDataType type)
+        {
+            return new LogCurveInfo()
+            {
+                Mnemonic = new ShortNameStruct(name),
+                TypeLogData = type,
+                Unit = unit
+            };
+        }
+
+        public string Units(List<LogCurveInfo> infoList)
+        {
+            return infoList != null
+                ? String.Join(",", infoList.Select(x => x.Unit ?? string.Empty))
+                : string.Empty;
+        }
+
+        public string Mnemonics(List<LogCurveInfo> infoList)
+        {
+            return infoList != null
+                ? String.Join(",", infoList.Select(x => x.Mnemonic))
+                : string.Empty;
+        }
+
+        public List<string> GenerateLogData(LogIndexType indexType, List<LogCurveInfo> logCurveInfoList, LogIndexDirection direction, int numOfRows = 5)
+        {           
+            const int Seed = 123;
+
+            Random random = new Random(Seed);
+            DateTime dateTimeStart = DateTime.Now.ToUniversalTime();
+            double interval = direction == LogIndexDirection.decreasing ? -1.0 : 1.0;
+
+            List<string> data = new List<string>();
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                string row = string.Empty;
+
+                // index value
+                switch (indexType)
+                {
+                    case LogIndexType.datetime:
+                        {
+                            dateTimeStart = dateTimeStart.AddSeconds(random.Next(1, 5));
+                            row += "\"" + dateTimeStart.AddSeconds(1.0).ToString("o") + "\"";
+                            break;
+                        }
+                    case LogIndexType.elapsedtime:                       
+                    case LogIndexType.length:                        
+                    case LogIndexType.measureddepth:                        
+                    case LogIndexType.verticaldepth:
+                        {
+                            row += string.Format("{0:F3}", i * interval);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+
+                
+                // channel values
+                for (int k = 1; k < logCurveInfoList.Count; k++)
+                {
+                    row += ", ";
+
+                    if (random.Next(1, 10) == 2)
+                    {
+                        continue;
+                    }
+
+                    switch (logCurveInfoList[k].TypeLogData)
+                    {
+                        case LogDataType.@byte:
+                            {
+                                row += "Y";
+                                break;
+                            }
+                        case LogDataType.datetime:
+                            {
+                                dateTimeStart = dateTimeStart.AddSeconds(random.Next(1, 5));
+                                row += "\"" + dateTimeStart.AddSeconds(1.0).ToString("o") + "\"";
+                                break;
+                            }
+                        case LogDataType.@double:
+                        case LogDataType.@float:                       
+                            {
+                                row += random.NextDouble().ToString("N3");
+                                break;
+                            }
+                        case LogDataType.@int:
+                        case LogDataType.@long:
+                        case LogDataType.@short:
+                            {
+                                row += random.Next(1, 10);
+                                break;
+                            }
+                        case LogDataType.@string:
+                            {
+                                row += "abc";
+                                break;
+                            }
+                        default:
+                            {
+                                row += "null";
+                            }
+                            break;
+                    }
+                }
+                data.Add(row);
+            }
+
+            return data;
+        }
+
+        public Log CreateLog(Log log, LogIndexDirection direction, int numOfRows = 5)
+        {
+            log.LogCurveInfo = new List<LogCurveInfo>();
+            
+            LogIndexType indexType = log.IndexType.HasValue ? log.IndexType.Value : LogIndexType.measureddepth;
+            if (DepthIndexTypes.Contains(indexType))
+            {
+                log.LogCurveInfo.Add(CreateDoubleLogCurveInfo("MD", "m"));
+            }
+            else
+            {
+                log.LogCurveInfo.Add(CreateDateTimeLogCurveInfo("DateTime", "s"));
+            }
+            log.LogCurveInfo.Add(CreateDoubleLogCurveInfo("GR", "api"));
+            log.LogCurveInfo.Add(CreateStringLogCurveInfo("UNK", "s"));
+
+            log.LogData = new List<LogData>() { new LogData() };
+        
+            log.LogData[0].Data = new List<string>();
+ 
+            log.LogData[0].MnemonicList = Mnemonics(log.LogCurveInfo);
+            log.LogData[0].UnitList = Units(log.LogCurveInfo);
+
+            log.LogData[0].Data = GenerateLogData(indexType, log.LogCurveInfo, direction, numOfRows);
+
+            return log;
+        }
+    }
+}
