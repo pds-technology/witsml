f58d8d4f9c3555cd07fa0b4e107e791fa72b3f36
jpuccia <joseph.puccia@pds.nl>
2016-03-23 12:02:33 -0500

    1) Added a helper method to get 2.0 Log Data for testing. 2) Modified sort index for ChannelSetValues 3) Fixed some issues when creating ChannelSetValues for 2.0 Log Data     - Conversions to Double were failing with a cast     - Indices needed to be cloned     - Last chunk was left off because of invalid loop test. 4) Renamed Log200DataAdapter tests to use same naming convension as other test classes.
    Related Work Items: #4981

--- src/Witsml.Server.IntegrationTest/Data/Logs/Log200DataAdapterTests.cs
+++ src/Witsml.Server.IntegrationTest/Data/Logs/Log200DataAdapterTests.cs
@@ -1,10 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Energistics.DataAccess.WITSML200;
 using Energistics.DataAccess.WITSML200.ComponentSchemas;
 using Energistics.DataAccess.WITSML200.ReferenceData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Newtonsoft.Json;
 using PDS.Framework;
 using PDS.Witsml.Server.Data.Channels;
 using PDS.Witsml.Server.Data.Wellbores;
@@ -26,6 +28,7 @@ namespace PDS.Witsml.Server.Data.Logs
         private Wellbore Wellbore1;
         private Log Log1;
         private Log Log2;
+        private Log LogDecreasing;
         private DataObjectReference WellReference;
         private DataObjectReference WellboreReference;
 
@@ -61,13 +64,15 @@ namespace PDS.Witsml.Server.Data.Logs
 
             Log1 = new Log() { Citation = DevKit.Citation("Log 01"), Wellbore = WellboreReference, Uuid = DevKit.Uid() };
             Log2 = new Log() { Citation = DevKit.Citation("Log 02"), Wellbore = WellboreReference };
+            LogDecreasing = new Log() { Citation = DevKit.Citation("Log Decreasing"), Wellbore = WellboreReference, Uuid = DevKit.Uid() };
 
             DevKit.InitHeader(Log1, LoggingMethod.MWD, ChannelIndexType.measureddepth);
             DevKit.InitHeader(Log2, LoggingMethod.Surface, ChannelIndexType.datetime);
+            DevKit.InitHeader(LogDecreasing, LoggingMethod.Surface, ChannelIndexType.measureddepth, IndexDirection.decreasing);
         }
 
         [TestMethod]
-        public void CanAddAndGetSingleLogWithUuid()
+        public void Log_can_be_added_with_uuid()
         {
             WellAdapter.Put(Well1);
             WellboreAdapter.Put(Wellbore1);
@@ -79,7 +84,7 @@ namespace PDS.Witsml.Server.Data.Logs
         }
 
         [TestMethod]
-        public void CanAddAndGetSingleLogWithoutUuid()
+        public void Log_can_be_added_without_uuid()
         {
             WellAdapter.Put(Well1);
             WellboreAdapter.Put(Wellbore1);
@@ -93,19 +98,92 @@ namespace PDS.Witsml.Server.Data.Logs
         }
 
         [TestMethod]
-        public void CanAddAndGetSingleLogWithSecondaryIndex()
+        public void Log_can_be_added_with_secondary_index()
         {
             var secondaryIndex = DevKit.ChannelIndex(ChannelIndexType.datetime);
             var channelSet = Log1.ChannelSet.First();
             channelSet.Index.Add(secondaryIndex);
+            
             DevKit.CreateMockChannelSetData(channelSet, channelSet.Index);
+
             WellAdapter.Put(Well1);
             WellboreAdapter.Put(Wellbore1);
             LogAdapter.Put(Log1);
 
+
             var log1 = LogAdapter.Get(Log1.GetObjectId());
 
             Assert.AreEqual(Log1.Citation.Title, log1.Citation.Title);
+        }
+
+        [TestMethod]
+        public void Log_can_be_added_with_increasing_log_data()
+        {
+            var numDataValue = 150;
+            var secondaryIndex = DevKit.ChannelIndex(ChannelIndexType.datetime);
+            var channelSet = Log1.ChannelSet.First();
+            channelSet.Index.Add(secondaryIndex);
+
+            // Save the Well and Wellbore
+            WellAdapter.Put(Well1);
+            WellboreAdapter.Put(Wellbore1);
+
+            // Generate 150 rows of data
+            DevKit.GenerateChannelData(Log1.ChannelSet, numDataValue);
+            LogAdapter.Put(Log1);
+
+            var cda = new ChannelDataAdapter(Provider);
+
+            // Retrieve the data
+            var uidLog = Log1.Uuid;
+            var mnemonics = Log1.ChannelSet.First().Channel.Select(c => c.Mnemonic).ToList();
+            var logData = cda.GetData(uidLog, mnemonics, null, true);
+
+            var rowCount = logData.Sum(ld => DevKit.DeserializeChannelSetData(ld.Data).Count);
+
+            var start = logData.First().Indices.First().Start;
+            var end = logData.Last().Indices.First().End;
+
+            // Test that the rows of data before and after are the same.
+            Assert.AreEqual(numDataValue, rowCount);
+
+            // Test the log is still increasing
+            Assert.IsTrue(end > start);
+        }
+
+        [TestMethod]
+        public void Log_can_be_added_with_decreasing_log_data()
+        {
+            var numDataValue = 150;
+            var secondaryIndex = DevKit.ChannelIndex(ChannelIndexType.datetime);
+            var channelSet = LogDecreasing.ChannelSet.First();
+            channelSet.Index.Add(secondaryIndex);
+
+            // Save the Well and Wellbore
+            WellAdapter.Put(Well1);
+            WellboreAdapter.Put(Wellbore1);
+
+            // Generate 150 rows of data
+            DevKit.GenerateChannelData(LogDecreasing.ChannelSet, numDataValue);
+            LogAdapter.Put(LogDecreasing);
+
+            var cda = new ChannelDataAdapter(Provider);
+
+            // Retrieve the data
+            var uidLog = LogDecreasing.Uuid;
+            var mnemonics = LogDecreasing.ChannelSet.First().Channel.Select(c => c.Mnemonic).ToList();
+            var logData = cda.GetData(uidLog, mnemonics, null, false);
+
+            var rowCount = logData.Sum(ld => DevKit.DeserializeChannelSetData(ld.Data).Count);
+
+            var start = logData.First().Indices.First().Start;
+            var end = logData.Last().Indices.First().End;
+
+            // Test that the rows of data before and after are the same.
+            Assert.AreEqual(numDataValue, rowCount);
+
+            // Test the log is still decreasing
+            Assert.IsTrue(end < start);
         }
     }
 }
