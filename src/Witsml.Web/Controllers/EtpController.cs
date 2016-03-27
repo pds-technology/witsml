e026423cd070c1c9233ec4acc57080970ed63b95
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-27 02:36:40 -0500

    Implemented ChannelStreaming Describe message for Log channel metadata.
    Related Work Items: #4768

--- src/Witsml.Web/Controllers/EtpController.cs
+++ src/Witsml.Web/Controllers/EtpController.cs
@@ -17,6 +17,7 @@ using Energistics.Protocol.Discovery;
 using Energistics.Protocol.Store;
 using PDS.Framework;
 using PDS.Witsml.Server.Properties;
+using PDS.Witsml.Server.Providers.ChannelStreaming;
 
 namespace PDS.Witsml.Web.Controllers
 {
@@ -113,8 +114,10 @@ namespace PDS.Witsml.Web.Controllers
         private EtpServerHandler CreateEtpServerHandler(WebSocket socket)
         {
             var handler = new EtpServerHandler(socket, DefaultServerName, DefaultServerVersion);
+            var aggregator = _container.Resolve<ChannelStreamingAggregator>();
 
-            handler.Register(() => _container.Resolve<IChannelStreamingProducer>());
+            handler.Register<IChannelStreamingProducer>(() => aggregator);
+            handler.Register<IChannelStreamingConsumer>(() => aggregator);
             handler.Register(() => _container.Resolve<IDiscoveryStore>());
             handler.Register(() => _container.Resolve<IStoreStore>());
 
