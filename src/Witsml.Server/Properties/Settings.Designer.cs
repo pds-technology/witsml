3e1cdc62678fd72ab424c03533cb1fce9046675d
Joel Romero <joel.romero@pds.nl>
2016-03-28 17:10:28 -0500

    Increase MaxDataNodes to 10000 and MaxDataPoints to 100000
    Related Work Items: #5031

--- src/Witsml.Server/Properties/Settings.Designer.cs
+++ src/Witsml.Server/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace PDS.Witsml.Server.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10000")]
         public int MaxDataNodes {
             get {
                 return ((int)(this["MaxDataNodes"]));
@@ -34,7 +34,7 @@ namespace PDS.Witsml.Server.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("100000")]
         public int MaxDataPoints {
             get {
                 return ((int)(this["MaxDataPoints"]));
