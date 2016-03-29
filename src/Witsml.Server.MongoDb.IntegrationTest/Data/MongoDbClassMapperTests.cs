ee8965531732146c9e20421e24181bd5003d9f3c
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-29 14:20:25 -0500

    Added Id member mapping and integration test for ChannelIndex type.
    Related Work Items: #4988

---
+++ src/Witsml.Server.MongoDb.IntegrationTest/Data/MongoDbClassMapperTests.cs
@@ -0,0 +1,28 @@
+﻿using Energistics.DataAccess.WITSML200.ComponentSchemas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson.Serialization;
+
+namespace PDS.Witsml.Server.Data
+{
+    [TestClass]
+    public class MongoDbClassMapperTests
+    {
+        private MongoDbClassMapper Mapper;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            Mapper = new MongoDbClassMapper();
+            Mapper.Register();
+        }
+
+        [TestMethod]
+        public void Mapper_can_provide_id_mapping_details()
+        {
+            var classMap = BsonClassMap.LookupClassMap(typeof(ChannelIndex));
+
+            Assert.IsNotNull(classMap.IdMemberMap);
+            Assert.AreEqual("Mnemonic", classMap.IdMemberMap.MemberName);
+        }
+    }
+}
