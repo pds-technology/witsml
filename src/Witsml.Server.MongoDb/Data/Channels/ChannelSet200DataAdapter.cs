f4521deafd468e54afbc164f0c5c495040ffe384
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-29 16:42:37 -0500

    Enabled channel streaming for 2.0 Channel Set data.
    Related Work Items: #4768

--- src/Witsml.Server.MongoDb/Data/Channels/ChannelSet200DataAdapter.cs
+++ src/Witsml.Server.MongoDb/Data/Channels/ChannelSet200DataAdapter.cs
@@ -2,10 +2,14 @@
 using System.ComponentModel.Composition;
 using System.Linq;
 using Energistics.DataAccess.WITSML200;
+using Energistics.DataAccess.WITSML200.ComponentSchemas;
+using Energistics.DataAccess.WITSML200.ReferenceData;
 using Energistics.Datatypes;
+using Energistics.Datatypes.ChannelData;
+using PDS.Framework;
 using PDS.Witsml.Data.Channels;
-using PDS.Witsml.Server.Configuration;
 using PDS.Witsml.Server.Models;
+using PDS.Witsml.Server.Providers;
 
 namespace PDS.Witsml.Server.Data.Channels
 {
@@ -13,10 +17,12 @@ namespace PDS.Witsml.Server.Data.Channels
     /// Data adapter that encapsulates CRUD functionality for <see cref="ChannelSet" />
     /// </summary>
     /// <seealso cref="PDS.Witsml.Server.Data.MongoDbDataAdapter{Energistics.DataAccess.WITSML200.ChannelSet}" />
+    /// <seealso cref="PDS.Witsml.Server.Data.Channels.IChannelDataProvider" />
     [Export(typeof(IEtpDataAdapter<ChannelSet>))]
     [Export200(ObjectTypes.ChannelSet, typeof(IEtpDataAdapter))]
+    [Export200(ObjectTypes.ChannelSet, typeof(IChannelDataProvider))]
     [PartCreationPolicy(CreationPolicy.Shared)]
-    public class ChannelSet200DataAdapter : MongoDbDataAdapter<ChannelSet>
+    public class ChannelSet200DataAdapter : MongoDbDataAdapter<ChannelSet>, IChannelDataProvider
     {
         private readonly ChannelDataChunkAdapter _channelDataChunkAdapter;
 
@@ -29,6 +35,52 @@ namespace PDS.Witsml.Server.Data.Channels
         public ChannelSet200DataAdapter(IDatabaseProvider databaseProvider, ChannelDataChunkAdapter channelDataChunkAdapter) : base(databaseProvider, ObjectNames.ChannelSet200, ObjectTypes.Uuid)
         {
             _channelDataChunkAdapter = channelDataChunkAdapter;
+        }
+
+        /// <summary>
+        /// Gets the channel metadata for the specified data object URI.
+        /// </summary>
+        /// <param name="uri">The parent data object URI.</param>
+        /// <returns>A collection of channel metadata.</returns>
+        public IList<ChannelMetadataRecord> GetChannelMetadata(EtpUri uri)
+        {
+            var entity = GetEntity(uri.ToDataObjectId());
+            var metadata = new List<ChannelMetadataRecord>();
+            var index = 0;
+
+            if (entity.Channel == null || !entity.Channel.Any())
+                return metadata;
+
+            metadata.AddRange(entity.Index.Select(x =>
+            {
+                var channel = ToChannelMetadataRecord(entity, x);
+                channel.ChannelId = index++;
+                return channel;
+            }));
+
+            metadata.AddRange(entity.Channel.Select(x =>
+            {
+                var channel = ToChannelMetadataRecord(entity, x);
+                channel.ChannelId = index++;
+                return channel;
+            }));
+
+            return metadata;
+        }
+
+        /// <summary>
+        /// Gets the channel data records for the specified data object URI and range.
+        /// </summary>
+        /// <param name="uri">The parent data object URI.</param>
+        /// <param name="range">The data range to retrieve.</param>
+        /// <returns>A collection of channel data.</returns>
+        public IEnumerable<IChannelDataRecord> GetChannelData(EtpUri uri, Range<double?> range)
+        {
+            var entity = GetEntity(uri.ToDataObjectId());
+            var indexChannel = entity.Index.FirstOrDefault();
+            var increasing = indexChannel.Direction.GetValueOrDefault() == IndexDirection.increasing;
+            var chunks = _channelDataChunkAdapter.GetData(uri, indexChannel.Mnemonic, range, increasing);
+            return chunks.GetRecords(range, increasing);
         }
 
         /// <summary>
@@ -116,6 +168,48 @@ namespace PDS.Witsml.Server.Data.Channels
             existing.Data = channelData;
 
             return reader;
+        }
+
+        private ChannelMetadataRecord ToChannelMetadataRecord(ChannelSet entity, ChannelIndex index)
+        {
+            var uri = index.GetUri(entity);
+
+            return new ChannelMetadataRecord()
+            {
+                ChannelUri = uri,
+                ContentType = uri.ContentType,
+                DataType = EtpDataType.@double.ToString().Replace("@", string.Empty),
+                Description = index.Mnemonic,
+                Mnemonic = index.Mnemonic,
+                Uom = index.Uom,
+                MeasureClass = ObjectTypes.Unknown,
+                Source = ObjectTypes.Unknown,
+                Uuid = index.Mnemonic,
+                Status = ChannelStatuses.Active,
+                ChannelAxes = new List<ChannelAxis>(),
+                Indexes = new List<IndexMetadataRecord>()
+            };
+        }
+
+        private ChannelMetadataRecord ToChannelMetadataRecord(ChannelSet entity, Channel channel)
+        {
+            var uri = channel.GetUri(entity);
+
+            return new ChannelMetadataRecord()
+            {
+                ChannelUri = uri,
+                ContentType = uri.ContentType,
+                DataType = channel.DataType.GetValueOrDefault(EtpDataType.@double).ToString().Replace("@", string.Empty),
+                Description = channel.Citation != null ? channel.Citation.Description ?? channel.Mnemonic : channel.Mnemonic,
+                Mnemonic = channel.Mnemonic,
+                Uom = channel.UoM,
+                MeasureClass = channel.CurveClass ?? ObjectTypes.Unknown,
+                Source = channel.Source ?? ObjectTypes.Unknown,
+                Uuid = channel.Mnemonic,
+                Status = ChannelStatuses.Active,
+                ChannelAxes = new List<ChannelAxis>(),
+                Indexes = new List<IndexMetadataRecord>()
+            };
         }
     }
 }
