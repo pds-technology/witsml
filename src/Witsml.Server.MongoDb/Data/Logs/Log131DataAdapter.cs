c7a6ec22746b3441c924d50c3d8992e3a501d98b
jpuccia <joseph.puccia@pds.nl>
2016-03-29 10:54:38 -0500

    Fixed Log query bug when there are no logs in the database.
    Related Work Items: #4866

--- src/Witsml.Server.MongoDb/Data/Logs/Log131DataAdapter.cs
+++ src/Witsml.Server.MongoDb/Data/Logs/Log131DataAdapter.cs
@@ -80,14 +80,17 @@ namespace PDS.Witsml.Server.Data.Logs
             if (!OptionsIn.ReturnElements.HeaderOnly.Equals(returnElements) && !OptionsIn.ReturnElements.IdOnly.Equals(returnElements) &&
                 !OptionsIn.RequestObjectSelectionCapability.True.Equals(parser.RequestObjectSelectionCapability()))
             {
-                var logHeaders = GetEntities(logs.Select(x => x.GetObjectId()))
-                    .ToDictionary(x => x.GetObjectId());
-
-                logs.ForEach(l =>
+                if (logs.Count > 0)
                 {
-                    var logHeader = logHeaders[l.GetObjectId()];
-                    l.LogData = QueryLogDataValues(logHeader, parser);
-                });
+                    var logHeaders = GetEntities(logs.Select(x => x.GetObjectId()))
+                        .ToDictionary(x => x.GetObjectId());
+
+                    logs.ForEach(l =>
+                    {
+                        var logHeader = logHeaders[l.GetObjectId()];
+                        l.LogData = QueryLogDataValues(logHeader, parser);
+                    });
+                }
             }
 
             return new WitsmlResult<IEnergisticsCollection>(
