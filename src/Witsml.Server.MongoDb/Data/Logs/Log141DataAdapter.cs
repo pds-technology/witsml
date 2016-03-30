3c984a7459a0018e3b016365ecca77a9df73f3c6
Bobby Diaz <bobby.diaz@pds.nl>
2016-03-30 09:10:57 -0500

    Fixed bug in GetMnemonicList when returnElements=id-only and logCurveInfo is null.
    Related Work Items: #5044

--- src/Witsml.Server.MongoDb/Data/Logs/Log141DataAdapter.cs
+++ src/Witsml.Server.MongoDb/Data/Logs/Log141DataAdapter.cs
@@ -322,6 +322,9 @@ namespace PDS.Witsml.Server.Data.Logs
 
         private IDictionary<int, string> GetMnemonicList(Log log, WitsmlQueryParser parser)
         {
+            if (log.LogCurveInfo == null)
+                return new Dictionary<int, string>(0);
+
             var allMnemonics = log.LogCurveInfo.Select(x => x.Mnemonic.Value).ToArray();
             var queryMnemonics = GetLogCurveInfoMnemonics(parser).ToArray();
 
