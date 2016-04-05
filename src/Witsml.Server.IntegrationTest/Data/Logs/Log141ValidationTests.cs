d8063360f7af2659ce02ee828ce8167100af0637
Cristina Ip <cristina.ip@pds.nl>
2016-04-05 17:20:30 -0500

    Added validation tests for AddToStore log data.
    Related Work Items: #4863

---
+++ src/Witsml.Server.IntegrationTest/Data/Logs/Log141ValidationTests.cs
@@ -0,0 +1,85 @@
+﻿//----------------------------------------------------------------------- 
+// PDS.Witsml.Server, 2016.1
+//
+// Copyright 2016 Petrotechnical Data Systems
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System.Linq;
+using Energistics.DataAccess.WITSML141;
+using Energistics.DataAccess.WITSML141.ComponentSchemas;
+using Energistics.DataAccess.WITSML141.ReferenceData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDS.Witsml.Server.Data.Logs
+{
+    [TestClass]
+    public class Log141ValidationTests
+    {
+        private DevKit141Aspect DevKit;
+        private Well Well;
+        private Wellbore Wellbore;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            DevKit = new DevKit141Aspect();
+
+            DevKit.Store.CapServerProviders = DevKit.Store.CapServerProviders
+                .Where(x => x.DataSchemaVersion == OptionsIn.DataVersion.Version141.Value)
+                .ToArray();
+
+            Well = new Well { Name = DevKit.Name("Well 01"), TimeZone = DevKit.TimeZone };
+
+            Wellbore = new Wellbore()
+            {
+                NameWell = Well.Name,
+                Name = DevKit.Name("Wellbore 01")
+            };
+        }
+
+        [TestMethod]
+        public void Test_add_error_code_463_nodes_with_same_index()
+        {
+            var response = DevKit.Add<WellList, Well>(Well);
+
+            Wellbore.UidWell = response.SuppMsgOut;
+            response = DevKit.Add<WellboreList, Wellbore>(Wellbore);
+
+            var log = new Log()
+            {
+                UidWell = Wellbore.UidWell,
+                NameWell = Well.Name,
+                UidWellbore = response.SuppMsgOut,
+                NameWellbore = Wellbore.Name,
+                Name = DevKit.Name("Log 01"),
+                LogData = DevKit.List(new LogData() { Data = DevKit.List<string>() })
+            };
+
+            var logData = log.LogData.First();
+            logData.Data.Add("13,13.1,");
+            logData.Data.Add("14,14.1,");
+            logData.Data.Add("15,15.1,");
+            logData.Data.Add("15,16.1,");
+            logData.Data.Add("17,17.1,");
+            logData.Data.Add("21,,21.2");
+            logData.Data.Add("22,,22.2");
+            logData.Data.Add("23,,23.2");
+            DevKit.InitHeader(log, LogIndexType.measureddepth);
+
+            response = DevKit.Add<LogList, Log>(log);
+            Assert.AreEqual((short)ErrorCodes.NodesWithSameIndex, response.Result);
+        }
+    }
+}
