3c6d591578f6f33e68deb91ca95443e353264684
Haifeng Chen <haifeng.chen@pds.nl>
2016-04-27 10:00:29 -0500

    Initial implement of MongoTransaction (commit and rollback)
    Related Work Items: #5421

--- src/Witsml.Server.IntegrationTest/Data/Logs/Log141DataAdapterUpdateTests.cs
+++ src/Witsml.Server.IntegrationTest/Data/Logs/Log141DataAdapterUpdateTests.cs
@@ -16,6 +16,9 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System.Linq;
+using Energistics.DataAccess.WITSML141;
+using Energistics.DataAccess.WITSML141.ReferenceData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PDS.Witsml.Server.Data.Logs
@@ -26,9 +29,109 @@ namespace PDS.Witsml.Server.Data.Logs
     [TestClass]
     public class Log141DataAdapterUpdateTests
     {
+        private DevKit141Aspect DevKit;
+        private Well Well;
+        private Wellbore Wellbore;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            DevKit = new DevKit141Aspect();
+
+            DevKit.Store.CapServerProviders = DevKit.Store.CapServerProviders
+                .Where(x => x.DataSchemaVersion == OptionsIn.DataVersion.Version141.Value)
+                .ToArray();
+
+            Well = new Well { Name = DevKit.Name("Well 01"), TimeZone = DevKit.TimeZone };
+
+            Wellbore = new Wellbore()
+            {
+                NameWell = Well.Name,
+                Name = DevKit.Name("Wellbore 01")
+            };
+        }
+
         //[TestMethod]
         //public void Log141DataAdapter_MethodName_ExpectedBehavior()
         //{
         //}
+
+        [TestMethod]
+        public void Log141DataAdapter_UpdateInStore_Rollback_When_Updating_Invalid_Data()
+        {
+            var response = DevKit.Add<WellList, Well>(Well);
+            Wellbore.UidWell = response.SuppMsgOut;
+
+            response = DevKit.Add<WellboreList, Wellbore>(Wellbore);
+            var uidWellbore = response.SuppMsgOut;
+
+            var log = new Log()
+            {
+                UidWell = Wellbore.UidWell,
+                NameWell = Well.Name,
+                UidWellbore = uidWellbore,
+                NameWellbore = Wellbore.Name,
+                Name = DevKit.Name("Log 01")
+            };
+
+            DevKit.InitHeader(log, LogIndexType.measureddepth);
+            var logData = log.LogData.First();
+            logData.Data.Add("13,13.1,13.2");
+            logData.Data.Add("14,14.1,");
+            logData.Data.Add("15,15.1,15.2");
+
+            response = DevKit.Add<LogList, Log>(log);
+            Assert.AreEqual((short)ErrorCodes.Success, response.Result);
+
+            var uidLog = response.SuppMsgOut;
+
+            var query = new Log
+            {
+                Uid = uidLog,
+                UidWell = log.UidWell,
+                UidWellbore = log.UidWellbore
+            };
+
+            var results = DevKit.Query<LogList, Log>(query, optionsIn: OptionsIn.ReturnElements.All);
+            Assert.AreEqual(1, results.Count);            
+
+            var logAdded = results.First();
+            Assert.IsNull(logAdded.Description);
+
+            var logDataAdded = logAdded.LogData.First();
+            for (var i = 0; i < logData.Data.Count; i++)
+            {
+                Assert.AreEqual(logData.Data[i], logDataAdded.Data[i]);
+            }
+
+            var update = new Log()
+            {
+                Uid = uidLog,
+                UidWell = Wellbore.UidWell,
+                UidWellbore = uidWellbore,
+                Description = "Should not be updated"
+            };
+
+            DevKit.InitHeader(update, LogIndexType.measureddepth);
+            logData = update.LogData.First();          
+            logData.Data.Add("17,17.1,17.2");
+            logData.Data.Add("21,21.1,21.2");
+            logData.Data.Add("21,22.1,22.2");
+
+            var updateResponse = DevKit.Update<LogList, Log>(update);
+            Assert.AreEqual((short)ErrorCodes.NodesWithSameIndex, updateResponse.Result);
+
+            results = DevKit.Query<LogList, Log>(query, optionsIn: OptionsIn.ReturnElements.All);
+            Assert.AreEqual(1, results.Count);
+
+            var logUpdated = results.First();
+            Assert.IsNull(logUpdated.Description);
+
+            var logDataUpdated = logUpdated.LogData.First();
+            for (var i = 0; i < logDataAdded.Data.Count; i++)
+            {
+                Assert.AreEqual(logDataAdded.Data[i], logDataUpdated.Data[i]);
+            }
+        }
     }
 }
