0c8a9022af94fef3508639e1591a16529d23d677
Cristina Ip <cristina.ip@pds.nl>
2016-05-10 16:13:37 -0500

    Moved MongoDbUtility methods that are not related to MongoDb to a base class DataObjectNavigator which is inherited by MongoDbQuery and MongoDbUpdate classes.
    Related Work Items: #5537

---
+++ src/Witsml/Data/DataObjectNavigator.cs
@@ -0,0 +1,205 @@
+﻿//----------------------------------------------------------------------- 
+// PDS.Witsml, 2016.1
+//
+// Copyright 2016 Petrotechnical Data Systems
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Energistics.DataAccess;
+using PDS.Framework;
+
+namespace PDS.Witsml.Data
+{
+    public abstract class DataObjectNavigator<TContext> where TContext : DataObjectNavigationContext
+    {
+        protected DataObjectNavigator(TContext context)
+        {
+            Context = context;
+        }
+
+        public TContext Context { get; }
+
+        /// <summary>
+        /// Gets the property information for an element.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <param name="name">The name of the property.</param>
+        /// <returns>The property info for the element.</returns>
+        protected PropertyInfo GetPropertyInfoForAnElement(IEnumerable<PropertyInfo> properties, string name)
+        {
+            foreach (var prop in properties)
+            {
+                var elementAttribute = prop.GetCustomAttribute<XmlElementAttribute>();
+                if (elementAttribute != null)
+                {
+                    if (elementAttribute.ElementName.EqualsIgnoreCase(name))
+                        return prop;
+                }
+
+                var arrayAttribute = prop.GetCustomAttribute<XmlArrayAttribute>();
+                if (arrayAttribute != null)
+                {
+                    if (arrayAttribute.ElementName.EqualsIgnoreCase(name))
+                        return prop;
+                }
+
+                var attributeAttribute = prop.GetCustomAttribute<XmlAttributeAttribute>();
+                if (attributeAttribute != null)
+                {
+                    if (attributeAttribute.AttributeName.EqualsIgnoreCase(name))
+                        return prop;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Validates the uom/value pair for the element.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="uomProperty">The uom property.</param>
+        /// <param name="measureValue">The measure value.</param>
+        /// <returns>The uom value if valid.</returns>
+        /// <exception cref="WitsmlException"></exception>
+        protected string ValidateMeasureUom(XElement element, PropertyInfo uomProperty, string measureValue)
+        {
+            var xmlAttribute = uomProperty.GetCustomAttribute<XmlAttributeAttribute>();
+
+            // validation not needed if uom attribute is not defined
+            if (xmlAttribute == null)
+                return null;
+
+            var uomValue = element.Attributes()
+                .Where(x => x.Name.LocalName == xmlAttribute.AttributeName)
+                .Select(x => x.Value)
+                .FirstOrDefault();
+
+            // uom is required when a measure value is specified
+            if (!string.IsNullOrWhiteSpace(measureValue) && string.IsNullOrWhiteSpace(uomValue))
+            {
+                throw new WitsmlException(ErrorCodes.MissingUnitForMeasureData);
+            }
+
+            return uomValue;
+        }
+
+        /// <summary>
+        /// Gets the concrete type of the element.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="propType">Type of the property.</param>
+        /// <returns>The concrete type</returns>
+        protected Type GetConcreteType(XElement element, Type propType)
+        {
+            var xsiType = element.Attributes()
+                .Where(x => x.Name == Xsi("type"))
+                .Select(x => x.Value.Split(':'))
+                .FirstOrDefault();
+
+            var @namespace = element.Attributes()
+                .Where(x => x.Name == Xmlns(xsiType.FirstOrDefault()))
+                .Select(x => x.Value)
+                .FirstOrDefault();
+
+            var typeName = xsiType.LastOrDefault();
+
+            return propType.Assembly.GetTypes()
+                .FirstOrDefault(t =>
+                {
+                    var xmlType = t.GetCustomAttribute<XmlTypeAttribute>();
+                    return ((xmlType != null && xmlType.TypeName == typeName) &&
+                        (string.IsNullOrWhiteSpace(@namespace) || xmlType.Namespace == @namespace));
+                });
+        }
+
+        protected object ParseEnum(Type enumType, string enumValue)
+        {
+            if (Enum.IsDefined(enumType, enumValue))
+            {
+                return Enum.Parse(enumType, enumValue);
+            }
+
+            var enumMember = enumType.GetMembers().FirstOrDefault(x =>
+            {
+                if (x.Name.EqualsIgnoreCase(enumValue))
+                    return true;
+
+                var xmlEnumAttrib = x.GetCustomAttribute<XmlEnumAttribute>();
+                return xmlEnumAttrib != null && xmlEnumAttrib.Name.EqualsIgnoreCase(enumValue);
+            });
+
+            // must be a valid enumeration member
+            if (!enumType.IsEnum || enumMember == null)
+            {
+                throw new WitsmlException(ErrorCodes.InvalidUnitOfMeasure);
+            }
+
+            return Enum.Parse(enumType, enumMember.Name);
+        }
+
+        protected IList<PropertyInfo> GetPropertyInfo(Type t)
+        {
+            return t.GetProperties()
+                .Where(p => !p.IsDefined(typeof(XmlIgnoreAttribute), false))
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets the Mongo collection field path for the property.
+        /// </summary>
+        /// <param name="parentPath">The parent path.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>The Mongo collection field path for the property.</returns>
+        protected string GetPropertyPath(string parentPath, string propertyName)
+        {
+            var prefix = string.IsNullOrEmpty(parentPath) ? string.Empty : string.Format("{0}.", parentPath);
+            return string.Format("{0}{1}", prefix, CaptalizeString(propertyName));
+        }
+
+        /// <summary>
+        /// Captalizes the string.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
+        protected string CaptalizeString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var result = char.ToUpper(input[0]).ToString();
+
+            if (input.Length > 1)
+                result += input.Substring(1);
+
+            return result;
+        }
+
+        protected XName Xmlns(string attributeName)
+        {
+            return XNamespace.Xmlns.GetName(attributeName);
+        }
+
+        protected XName Xsi(string attributeName)
+        {
+            return WitsmlParser.Xsi.GetName(attributeName);
+        }
+
+    }
+}
