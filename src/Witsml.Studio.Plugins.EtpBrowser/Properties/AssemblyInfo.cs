368442359134ee63845fd9b695eb43adeb817efd
Bobby Diaz <bobby.diaz@pds.nl>
2016-05-19 15:01:48 -0500

    Added .nuspec files for core libraries and updated AssemblyInfos to include proper metadata.
    Related Work Items: #5733

--- src/Witsml.Studio.Plugins.EtpBrowser/Properties/AssemblyInfo.cs
+++ src/Witsml.Studio.Plugins.EtpBrowser/Properties/AssemblyInfo.cs
@@ -17,8 +17,6 @@
 //-----------------------------------------------------------------------
 
 using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -26,9 +24,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PDS.Witsml.Studio.Plugins.EtpBrowser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PDS WITSML Studio ETP Browser Plug-in")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Petrotechnical Data Systems")]
 [assembly: AssemblyProduct("PDS.Witsml.Studio")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -69,5 +67,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2016.1.0.0")]
+[assembly: AssemblyFileVersion("2016.1.0.0")]
--- src/Witsml.Studio/Properties/AssemblyInfo.cs
+++ src/Witsml.Studio/Properties/AssemblyInfo.cs
@@ -17,7 +17,6 @@
 //-----------------------------------------------------------------------
 
 using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -26,9 +25,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PDS.Witsml.Studio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PDS WITSML Studio")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Petrotechnical Data Systems")]
 [assembly: AssemblyProduct("PDS.Witsml.Studio")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -69,8 +68,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2016.1.0.0")]
+[assembly: AssemblyFileVersion("2016.1.0.0")]
 
 [assembly: InternalsVisibleTo("PDS.Witsml.Studio.UnitTest")]
 [assembly: InternalsVisibleTo("PDS.Witsml.Studio.IntegrationTest")]
