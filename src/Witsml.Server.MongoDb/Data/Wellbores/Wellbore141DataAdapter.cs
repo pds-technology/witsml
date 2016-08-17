280af7883437f4659c02b3f58352172bcd176c47
Haifeng Chen <haifeng.chen@pds.nl>
2016-08-17 13:51:04 -0500

    Implement delete for wellbore and create integration tests
    Related Work Items: #6827

--- src/Witsml.Server.MongoDb/Data/Wellbores/Wellbore141DataAdapter.cs
+++ src/Witsml.Server.MongoDb/Data/Wellbores/Wellbore141DataAdapter.cs
@@ -101,5 +101,27 @@ namespace PDS.Witsml.Server.Data.Wellbores
         {
             return new List<string> { "isActive" };
         }
+
+        /// <summary>
+        /// Deletes or partially updates the specified object by uid.
+        /// </summary>
+        /// <param name="parser">The query parser that specifies the object.</param>
+        public override void Delete(WitsmlQueryParser parser)
+        {
+            var uri = parser.GetUri<Wellbore>();
+
+            if (IsPartialDelete(parser))
+            {
+                using (var transaction = DatabaseProvider.BeginTransaction(uri))
+                {
+                    PartialDeleteEntity(parser, uri, transaction);
+                    transaction.Commit();
+                }
+            }
+            else
+            {
+                Delete(uri);
+            }
+        }
     }
 }
