740293cb975764344525fb5e42d2ac6a0682acd8
Haifeng Chen <haifeng.chen@pds.nl>
2016-08-30 11:59:54 -0500

    Implement trajectory validation (AddToStore)
    Related Work Items: #6979

--- src/Witsml.Server/Data/Trajectories/Trajectory131Validator.cs
+++ src/Witsml.Server/Data/Trajectories/Trajectory131Validator.cs
@@ -16,7 +16,11 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Energistics.DataAccess.WITSML131;
+using PDS.Witsml.Server.Configuration;
 
 namespace PDS.Witsml.Server.Data.Trajectories
 {
@@ -25,5 +29,73 @@ namespace PDS.Witsml.Server.Data.Trajectories
     /// </summary>
     public partial class Trajectory131Validator
     {
+        /// <summary>
+        /// Configures the context.
+        /// </summary>
+        protected override void ConfigureContext()
+        {
+            Context.Ignored = new List<string>
+            {
+                "mdMin", "mdMx"
+            };
+        }
+
+        /// <summary>
+        /// Validates the data object while executing AddToStore.
+        /// </summary>
+        /// <returns>A collection of validation results.</returns>
+        protected override IEnumerable<ValidationResult> ValidateForInsert()
+        {
+            var uri = DataObject.GetUri();
+            var uriWellbore = uri.Parent;
+            var uriWell = uriWellbore.Parent;
+            var wellbore = WellboreDataAdapter.Get(uriWellbore);
+
+            var stations = DataObject.TrajectoryStation;
+
+            // Validate parent uid property
+            if (string.IsNullOrWhiteSpace(DataObject.UidWell))
+            {
+                yield return new ValidationResult(ErrorCodes.MissingElementUidForAdd.ToString(), new[] { "UidWell" });
+            }
+            // Validate parent uid property
+            else if (string.IsNullOrWhiteSpace(DataObject.UidWellbore))
+            {
+                yield return new ValidationResult(ErrorCodes.MissingElementUidForAdd.ToString(), new[] { "UidWellbore" });
+            }
+
+            // Validate parent exists
+            else if (!WellDataAdapter.Exists(uriWell))
+            {
+                yield return new ValidationResult(ErrorCodes.MissingParentDataObject.ToString(), new[] { "UidWell" });
+            }
+            // Validate parent exists
+            else if (wellbore == null)
+            {
+                yield return new ValidationResult(ErrorCodes.MissingParentDataObject.ToString(), new[] { "UidWellbore" });
+            }
+
+            else if (!wellbore.UidWell.Equals(DataObject.UidWell) || !wellbore.Uid.Equals(DataObject.UidWellbore))
+            {
+                yield return new ValidationResult(ErrorCodes.IncorrectCaseParentUid.ToString(), new[] { "UidWellbore" });
+            }
+
+            // Validate UID does not exist
+            else if (DataAdapter.Exists(uri))
+            {
+                yield return new ValidationResult(ErrorCodes.DataObjectUidAlreadyExists.ToString(), new[] { "Uid" });
+            }
+            else if (stations != null)
+            {
+                if (stations.Any(s => string.IsNullOrWhiteSpace(s.Uid)))
+                {
+                    yield return new ValidationResult(ErrorCodes.MissingElementUidForAdd.ToString(), new[] { "TrajectoryStation", "Uid" });
+                }
+                else if (stations.Count > WitsmlSettings.MaxDataNodes)
+                {
+                    yield return new ValidationResult(ErrorCodes.MaxDataExceeded.ToString(), new[] { "TrajectoryStation" });
+                }
+            }
+        }
     }
 }
