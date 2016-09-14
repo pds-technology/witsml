d2ac9937bf2596f2c002c29ae5c0a79732ba41dc
Haifeng Chen <haifeng.chen@pds.nl>
2016-09-14 08:25:00 -0500

    Create integration tests
    Related Work Items: #7130

--- src/Witsml.Server.IntegrationTest/Data/Trajectories/Trajectory131DataAdapterUpdateTests.cs
+++ src/Witsml.Server.IntegrationTest/Data/Trajectories/Trajectory131DataAdapterUpdateTests.cs
@@ -16,6 +16,9 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+using Energistics.DataAccess.WITSML131;
 using Energistics.DataAccess.WITSML131.ComponentSchemas;
 using Energistics.DataAccess.WITSML131.ReferenceData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,7 +31,7 @@ namespace PDS.Witsml.Server.Data.Trajectories
     public partial class Trajectory131DataAdapterUpdateTests
     {
         [TestMethod]
-        public void Trajectory141DataAdapter_UpdateInStore_Update_Trajectory_Header()
+        public void Trajectory131DataAdapter_UpdateInStore_Update_Trajectory_Header()
         {
             // Add well and wellbore
             AddParents();
@@ -48,6 +51,60 @@ namespace PDS.Witsml.Server.Data.Trajectories
             result = DevKit.GetAndAssert(Trajectory);
             Assert.AreEqual(AziRef.gridnorth, result.AziRef);
             Assert.IsNull(result.MagDeclUsed);
+        }
+
+        [TestMethod]
+        public void Trajectory131DataAdapter_UpdateInStore_Update_Trajectory_Data()
+        {
+            // Add well and wellbore
+            AddParents();
+
+            // Add trajectory without stations
+            var stations = DevKit.TrajectoryStations(5, 0);
+            var station5 = stations.LastOrDefault();
+            Assert.IsNotNull(station5);
+            stations.Remove(station5);
+            Trajectory.TrajectoryStation = stations;
+            DevKit.AddAndAssert(Trajectory);
+
+            // Get trajectory
+            var result = DevKit.GetAndAssert(Trajectory);
+            Assert.AreEqual(Trajectory.TrajectoryStation.Count, result.TrajectoryStation.Count);
+
+            var station1 = Trajectory.TrajectoryStation.FirstOrDefault();
+            Assert.IsNotNull(station1);
+            station1.Azi.Value++;
+
+            var station1Update = new TrajectoryStation
+            {
+                Uid = station1.Uid,
+                TypeTrajStation = station1.TypeTrajStation,
+                Azi = station1.Azi,
+                Location = new List<Location>
+                {
+                    new Location {Uid = "loc-1", Description = "location 1"}
+                }
+            };
+
+            var update = new Trajectory
+            {
+                Uid = Trajectory.Uid,
+                UidWell = Trajectory.UidWell,
+                UidWellbore = Trajectory.UidWellbore,
+                TrajectoryStation = new List<TrajectoryStation> { station1Update, station5 }
+            };
+
+            DevKit.UpdateAndAssert<TrajectoryList, Trajectory>(update);
+
+            result = DevKit.GetAndAssert(Trajectory);
+            Assert.AreEqual(5, result.TrajectoryStation.Count);
+            var updatedStation1 = result.TrajectoryStation.FirstOrDefault(s => s.Uid == station1.Uid);
+            Assert.IsNotNull(updatedStation1);
+            Assert.AreEqual(station1.Azi.Value, updatedStation1.Azi.Value);
+            Assert.AreEqual(station1Update.Location.Count, updatedStation1.Location.Count);
+
+            var updatedStation5 = result.TrajectoryStation.FirstOrDefault(s => s.Uid == station5.Uid);
+            Assert.IsNotNull(updatedStation5);
         }
     }
 }
--- src/Witsml.Server.IntegrationTest/Data/Trajectories/Trajectory141DataAdapterUpdateTests.cs
+++ src/Witsml.Server.IntegrationTest/Data/Trajectories/Trajectory141DataAdapterUpdateTests.cs
@@ -16,6 +16,9 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+using Energistics.DataAccess.WITSML141;
 using Energistics.DataAccess.WITSML141.ComponentSchemas;
 using Energistics.DataAccess.WITSML141.ReferenceData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -48,6 +51,57 @@ namespace PDS.Witsml.Server.Data.Trajectories
             result = DevKit.GetAndAssert(Trajectory);
             Assert.AreEqual(AziRef.gridnorth, result.AziRef);
             Assert.IsNull(result.MagDeclUsed);
+        }
+
+        [TestMethod]
+        public void Trajectory141DataAdapter_UpdateInStore_Update_Trajectory_Data()
+        {
+            // Add well and wellbore
+            AddParents();
+
+            // Add trajectory without stations
+            var stations = DevKit.TrajectoryStations(5, 0);
+            var station5 = stations.LastOrDefault();
+            Assert.IsNotNull(station5);
+            stations.Remove(station5);
+            Trajectory.TrajectoryStation = stations;
+            DevKit.AddAndAssert(Trajectory);
+
+            // Get trajectory
+            var result = DevKit.GetAndAssert(Trajectory);
+            Assert.AreEqual(Trajectory.TrajectoryStation.Count, result.TrajectoryStation.Count);
+
+            var station1 = Trajectory.TrajectoryStation.FirstOrDefault();
+            Assert.IsNotNull(station1);
+            station1.Azi.Value++;
+
+            var station1Update = new TrajectoryStation
+            {
+                Uid = station1.Uid,
+                TypeTrajStation = station1.TypeTrajStation,
+                Azi = station1.Azi,
+                ExtensionNameValue = new List<ExtensionNameValue> { DevKit.ExtensionNameValue("Ext-1", "1.0", "m") }
+            };
+
+            var update = new Trajectory
+            {
+                Uid = Trajectory.Uid,
+                UidWell = Trajectory.UidWell,
+                UidWellbore = Trajectory.UidWellbore,
+                TrajectoryStation = new List<TrajectoryStation> {station1Update, station5}
+            };
+
+            DevKit.UpdateAndAssert<TrajectoryList, Trajectory>(update);
+
+            result = DevKit.GetAndAssert(Trajectory);
+            Assert.AreEqual(5, result.TrajectoryStation.Count);
+            var updatedStation1 = result.TrajectoryStation.FirstOrDefault(s => s.Uid == station1.Uid);
+            Assert.IsNotNull(updatedStation1);
+            Assert.AreEqual(station1.Azi.Value, updatedStation1.Azi.Value);
+            Assert.AreEqual(station1Update.ExtensionNameValue.Count, updatedStation1.ExtensionNameValue.Count);
+
+            var updatedStation5 = result.TrajectoryStation.FirstOrDefault(s => s.Uid == station5.Uid);
+            Assert.IsNotNull(updatedStation5);
         }
     }
 }
