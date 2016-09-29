1c16a72d5d9b106efd50e8d94ef806250a1f7129
jpuccia <joseph.puccia@pds.nl>
2016-09-29 15:20:22 -0500

    Created test for Well131 Get
    Related Work Items: #7312

--- src/Witsml.Server.IntegrationTest/Data/Wells/Well131DataAdapterGetTests.cs
+++ src/Witsml.Server.IntegrationTest/Data/Wells/Well131DataAdapterGetTests.cs
@@ -16,6 +16,12 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System;
+using System.Linq;
+using Energistics.DataAccess.WITSML131;
+using Energistics.DataAccess.WITSML131.ComponentSchemas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace PDS.Witsml.Server.Data.Wells
 {
     /// <summary>
@@ -23,5 +29,38 @@ namespace PDS.Witsml.Server.Data.Wells
     /// </summary>
     public partial class Well131DataAdapterGetTests
     {
+        [TestMethod]
+        public void Well131DataAdapter_GetFromStore_Selection_Not_Equal_Comparison_dTimLastChange()
+        {
+            var well01 = DevKit.CreateFullWell();
+            var response = DevKit.Add<WellList, Well>(well01);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual((short)ErrorCodes.Success, response.Result);
+            var uid01 = response.SuppMsgOut;
+
+            var query = new Well { Uid = uid01 };
+            var result = DevKit.Query<WellList, Well>(query, ObjectTypes.Well, null, optionsIn: OptionsIn.ReturnElements.All);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(uid01, result[0].Uid);
+
+            var wellLastChangeTime = result[0].CommonData.DateTimeLastChange;
+
+            var well02 = DevKit.CreateFullWell();
+            well02.CommonData.DateTimeCreation = DateTimeOffset.UtcNow;
+            response = DevKit.Add<WellList, Well>(well02);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual((short)ErrorCodes.Success, response.Result);
+            var uid02 = response.SuppMsgOut;
+
+            query = new Well { CommonData = new CommonData() };
+            query.CommonData.DateTimeLastChange = wellLastChangeTime;
+            result = DevKit.Query<WellList, Well>(query, ObjectTypes.Well, null, optionsIn: OptionsIn.ReturnElements.All);
+
+            // Section 6.6.4
+            Assert.IsTrue(result.Any(x => x.Uid == uid02));
+            Assert.IsFalse(result.Any(x => x.Uid == uid01));
+        }
     }
 }
