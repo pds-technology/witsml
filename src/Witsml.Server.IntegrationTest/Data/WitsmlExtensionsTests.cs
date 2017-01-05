e779d4b6e395fae5a3a998c9fda3343fb7319a4b
Joseph Puccia <joseph.puccia@pds.nl>
2017-01-05 12:56:21 -0600

    Added test methods to test GetLogMaxNodes extension method

---
+++ src/Witsml.Server.IntegrationTest/Data/WitsmlExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDS.Witsml.Server.Configuration;
+
+namespace PDS.Witsml.Server.Data
+{
+    [TestClass]
+    public class WitsmlExtensionsTests
+    {
+        [TestMethod]
+        public void WitsmlExtensions_GetLogMaxNodes_GetFromStore_Returns_Value()
+        {
+            // Change the setting for LogMaxDataNodesGet to something we can check for
+            WitsmlSettings.LogMaxDataNodesGet = 20;
+
+            // Check that GetLogMaxNodes returns what is expected for GetFromStore
+            Assert.AreEqual(WitsmlSettings.LogMaxDataNodesGet, Functions.GetFromStore.GetLogMaxNodes());
+        }
+
+        [TestMethod]
+        public void WitsmlExtensions_GetLogMaxNodes_DeleteFromStore_Returns_Value()
+        {
+            // Change the setting for LogMaxDataNodesDelete to something we can check for
+            WitsmlSettings.LogMaxDataNodesDelete = 20;
+
+            // Check that GetLogMaxNodes returns what is expected for DeleteFromStore
+            Assert.AreEqual(WitsmlSettings.LogMaxDataNodesDelete, Functions.DeleteFromStore.GetLogMaxNodes());
+        }
+    }
+}
