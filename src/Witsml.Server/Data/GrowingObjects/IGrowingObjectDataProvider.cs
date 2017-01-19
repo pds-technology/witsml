5b21e35867a9f240c27d5746b0b94ba7fa53be42
Joseph Puccia <joseph.puccia@pds.nl>
2017-01-19 08:29:01 -0600

    Added an IGrowingObjectDataProvider to the Witsml.Server
    Related Work Items: #8591

---
+++ src/Witsml.Server/Data/GrowingObjects/IGrowingObjectDataProvider.cs
@@ -0,0 +1,45 @@
+﻿//----------------------------------------------------------------------- 
+// PDS.Witsml.Server, 2016.1
+//
+// Copyright 2016 Petrotechnical Data Systems
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
+using Energistics.Datatypes;
+
+namespace PDS.Witsml.Server.Data.GrowingObjects
+{
+    /// <summary>
+    /// Represents a data provider that supports growing object operations.
+    /// </summary>
+    public interface IGrowingObjectDataProvider
+    {
+        /// <summary>
+        /// Updates the last append date time for a growing object for the specified uri.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="wellboreUri">The wellbore URI.</param>
+        void UpdateLastAppendDateTime(EtpUri uri, EtpUri wellboreUri);
+
+        /// <summary>
+        /// Expires the growing objects for the specified objectType and expiredDateTime.
+        /// Any growing object of the specified type will have its objectGrowing flag set
+        /// to false if its lastAppendDateTime is older than the expireDateTime.
+        /// </summary>
+        /// <param name="objectType">Type of the groing object.</param>
+        /// <param name="expiredDateTime">The expired date time.</param>
+        void ExpireGrowingObjects(string objectType, DateTime expiredDateTime);
+    }
+}
