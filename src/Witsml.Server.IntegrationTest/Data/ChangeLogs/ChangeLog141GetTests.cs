21a9b2529656c7b6f84a12b0a5eaaaecd5a23aa8
Darshana Eeshwar <Darshana.Eeshwar@pds.nl>
2017-02-10 13:02:53 -0600

    Added GetFromStore tests for changeLog
    Related Work Items: #8900

---
+++ src/Witsml.Server.IntegrationTest/Data/ChangeLogs/ChangeLog141GetTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Energistics.DataAccess.WITSML141;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDS.Witsml.Server.Data.ChangeLogs
+{
+    [TestClass]
+    public class ChangeLog141GetTests : MultiObject141TestBase
+    {
+        public const string QueryEmptyRootTemplate = @"<changeLogs xmlns=""http://www.witsml.org/schemas/1series"" version=""1.4.1.1"">{0}</changeLogs>";       
+
+        [TestMethod]
+        public void ChangeLog141GetTests_GetFromStore_Can_Get_ChangeLogs_Without_ChangeHistory_With_Latest_Change_Only_OptionsIn()
+        {
+            AddParents();
+
+            DevKit.AddAndAssert(Log);
+            DevKit.AddAndAssert(Trajectory);
+
+            var queryIn = string.Format(QueryEmptyRootTemplate, "<changeLog />");
+            var changeLogs = GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.LatestChangeOnly);
+
+            changeLogs.ForEach(c => Assert.AreEqual(0, c.ChangeHistory.Count));
+        }
+
+        [TestMethod]
+        public void ChangeLog141GetTests_GetFromStore_Can_Get_ChangeLogs_With_DateTimeLastChange_Filter()
+        {
+            var dateFilter = "<changeLog><commonData><dTimLastChange>{0}</dTimLastChange></commonData></changeLog>";
+
+            AddParents();
+            var wellbore = DevKit.GetAndAssert<WellboreList, Wellbore>(Wellbore);
+            var dateTimeLastChangeAfterWellbore = wellbore.CommonData.DateTimeLastChange;
+
+            DevKit.AddAndAssert(Log);
+            DevKit.AddAndAssert(Trajectory);
+
+            var trajectory = DevKit.GetAndAssert<TrajectoryList, Trajectory>(Trajectory);
+            var datetimeLastChangeAfterTrajectory = trajectory.CommonData.DateTimeLastChange;
+
+            var queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateFilter, datetimeLastChangeAfterTrajectory));
+            GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.LatestChangeOnly, 0);
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateFilter, dateTimeLastChangeAfterWellbore));
+            var changeLogs = GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.LatestChangeOnly, 2);
+
+            // No change history returned
+            changeLogs.ForEach((c) => Assert.AreEqual(0, c.ChangeHistory.Count));
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateFilter, datetimeLastChangeAfterTrajectory));
+            GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 0);
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateFilter, dateTimeLastChangeAfterWellbore));
+            changeLogs = GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 2);
+
+            // Change history returned
+            changeLogs.ForEach((c) => Assert.AreEqual(1, c.ChangeHistory.Count));
+        }
+
+        [TestMethod]
+        public void ChangeLog141GetTests_GetFromStore_Can_Get_ChangeLogs_With_DateTimeLastChange_And_ObjectType_Filters()
+        {
+            var dateAndObjectTypeFilter = "<changeLog><objectType>{0}</objectType><commonData><dTimLastChange>{1}</dTimLastChange></commonData></changeLog>";
+
+            AddParents();
+            var wellbore = DevKit.GetAndAssert<WellboreList, Wellbore>(Wellbore);
+            var dateTimeLastChangeAfterWellbore = wellbore.CommonData.DateTimeLastChange;
+
+            DevKit.AddAndAssert(Log);
+
+            var queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateAndObjectTypeFilter, ObjectTypes.Trajectory, dateTimeLastChangeAfterWellbore));
+            GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 0);
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateAndObjectTypeFilter, ObjectTypes.Log, dateTimeLastChangeAfterWellbore));
+            GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 1);
+
+            DevKit.AddAndAssert(Trajectory);
+
+            var trajectory = DevKit.GetAndAssert<TrajectoryList, Trajectory>(Trajectory);
+            var datetimeLastChangeAfterTrajectory = trajectory.CommonData.DateTimeLastChange;
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateAndObjectTypeFilter, ObjectTypes.Trajectory, dateTimeLastChangeAfterWellbore));
+            var changeLogs = GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 1);
+
+            Assert.IsTrue(changeLogs.Count == 1);
+            Assert.IsTrue(changeLogs.First().ChangeHistory.Count == 1);
+            Assert.AreEqual(ObjectTypes.Trajectory, changeLogs.First().ObjectType);
+
+            queryIn = string.Format(QueryEmptyRootTemplate, string.Format(dateAndObjectTypeFilter, ObjectTypes.Trajectory, datetimeLastChangeAfterTrajectory));
+            GetAndAssertChangeLog(queryIn, OptionsIn.ReturnElements.All, 0);
+        }
+
+        private List<ChangeLog> GetAndAssertChangeLog(string queryIn, OptionsIn optionsIn, int? expectedChangeLogCount = null)
+        {
+            var changeLogs = DevKit.Query<ChangeLogList, ChangeLog>(ObjectTypes.ChangeLog, queryIn, null, optionsIn);
+
+            Assert.IsNotNull(changeLogs);
+
+            if (expectedChangeLogCount.HasValue)
+                Assert.AreEqual(expectedChangeLogCount, changeLogs.Count);
+
+            return changeLogs;
+        }
+
+    }
+}
