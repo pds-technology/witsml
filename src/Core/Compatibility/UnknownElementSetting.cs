3862a31989cb50bf9f6ff0bc35cc3b1a45d360dc
Bobby Diaz <bobby.diaz@pds.nl>
2017-03-09 09:44:13 -0600

    Fixed copyright product names and version numbers.
    Related Work Items: #9846

--- src/Core/Compatibility/UnknownElementSetting.cs
+++ src/Core/Compatibility/UnknownElementSetting.cs
@@ -1,5 +1,5 @@
 ﻿//----------------------------------------------------------------------- 
-// PDS.Witsml.Server, 2017.1
+// PDS.Witsml, 2017.1
 //
 // Copyright 2017 Petrotechnical Data Systems
 // 
