4dc69e4182d76f46e199bb2d4a2795c97b9490de
Joseph Puccia <joseph.puccia@pds.nl>
2017-03-16 13:54:30 -0500

    Beginning tests for new compatibility settings
    Related Work Items: #10272

--- src/Store.IntegrationTest/Data/Logs/Log131EtpTests.cs
+++ src/Store.IntegrationTest/Data/Logs/Log131EtpTests.cs
@@ -16,6 +16,14 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using Energistics.DataAccess.WITSML131;
+using Energistics.DataAccess.WITSML131.ReferenceData;
+using Energistics.Protocol;
+using Energistics.Protocol.Store;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDS.WITSMLstudio.Compatibility;
+
 namespace PDS.WITSMLstudio.Store.Data.Logs
 {
     /// <summary>
@@ -23,5 +31,38 @@ namespace PDS.WITSMLstudio.Store.Data.Logs
     /// </summary>
     public partial class Log131EtpTests
     {
+
+        [TestMethod, Description("Tests that 131 Log Data can be added when Compatibility Setting LogAllowPutObjectWithData is True")]
+        public async Task Log131_PutObject_Can_Add_Log_Data_With_LogAllowPutObjectWithData_True()
+        {
+            AddParents();
+
+            var numRows = 10;
+
+            // Allow for Log data to be saved during a Put
+            CompatibilitySettings.LogAllowPutObjectWithData = true;
+
+            await RequestSessionAndAssert();
+
+            var handler = _client.Handler<IStoreCustomer>();
+            var uri = Log.GetUri();
+            DevKit.InitHeader(Log, LogIndexType.measureddepth);
+            DevKit.InitDataMany(Log, DevKit.Mnemonics(Log), DevKit.Units(Log), numRows);
+
+            var dataObject = CreateDataObject<LogList, Log>(uri, Log);
+
+            // Put Object
+            await PutAndAssert(handler, dataObject);
+
+            // Get Object
+            var result = DevKit.GetAndAssert<LogList, Log>(DevKit.CreateLog(Log));
+
+            // Verify that the Log was saved
+            Assert.IsNotNull(result);
+
+            // Verify that the Log Data was saved.
+            Assert.IsNotNull(result.LogData);
+            Assert.AreEqual(numRows, result.LogData.Count);
+        }
     }
 }
