0f5eda1239d748a433ce8034d95f6909b4285948
Bobby Diaz <bobby.diaz@pds.nl>
2017-03-19 23:36:22 -0500

    Initial check-in of Store Notification implementation using Kafka as a message broker.

---
+++ Providers/StoreNotification/IStoreNotificationProducer.cs
@@ -0,0 +1,10 @@
+﻿namespace PDS.WITSMLstudio.Store.Providers.StoreNotification
+{
+    /// <summary>
+    /// Defines the methods that can be used to send change notification messages.
+    /// </summary>
+    public interface IStoreNotificationProducer
+    {
+        
+    }
+}
