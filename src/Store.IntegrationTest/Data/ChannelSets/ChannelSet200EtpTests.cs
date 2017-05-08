4502d3bef45686d53122075095355296f7d6d35a
Bobby Diaz <bobby.diaz@pds.nl>
2017-05-08 11:18:19 -0500

    Added ChannelSet test to add objects with run and hole size metadata specified.
    Related Work Items: #11727

--- src/Store.IntegrationTest/Data/ChannelSets/ChannelSet200EtpTests.cs
+++ src/Store.IntegrationTest/Data/ChannelSets/ChannelSet200EtpTests.cs
@@ -16,6 +16,14 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Energistics.DataAccess.WITSML200.ComponentSchemas;
+using Energistics.Protocol.Store;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace PDS.WITSMLstudio.Store.Data.ChannelSets
 {
     /// <summary>
@@ -23,5 +31,72 @@ namespace PDS.WITSMLstudio.Store.Data.ChannelSets
     /// </summary>
     public partial class ChannelSet200EtpTests
     {
+        [TestMethod]
+        public async Task ChannelSet200_PutObject_Can_Add_ChannelSet_With_Metadata()
+        {
+            AddParents();
+            await RequestSessionAndAssert();
+
+            var holeSizes = new[] { 17.5, 12.25, 8.5, 6.0 };
+            var timeDepths = new[] { "Depth", "Time" };
+            var runs = new List<Tuple<double, int, double, double>>
+            {
+                new Tuple<double, int, double, double>(17.5, 1, 0.0, 200.0),
+                new Tuple<double, int, double, double>(17.5, 2, 200.0, 435.0),
+                new Tuple<double, int, double, double>(12.25, 3, 435.0, 800.0),
+                new Tuple<double, int, double, double>(12.25, 4, 800.0, 1250.0),
+                new Tuple<double, int, double, double>(12.25, 5, 1250.0, 1400.0),
+                new Tuple<double, int, double, double>(8.5, 6, 1400.0, 1675.0),
+                new Tuple<double, int, double, double>(8.5, 7, 1675.0, 1900.0),
+                new Tuple<double, int, double, double>(8.5, 8, 1900.0, 2305.0),
+                new Tuple<double, int, double, double>(8.5, 9, 2305.0, 2700.0),
+                new Tuple<double, int, double, double>(6.0, 10, 2700.0, 3100.0),
+                new Tuple<double, int, double, double>(6.0, 11, 3100.0, 3450.0),
+            };
+
+            foreach (var holeSize in holeSizes)
+            {
+                foreach (var run in runs.Where(x => x.Item1.Equals(holeSize)))
+                {
+                    foreach (var timeDepth in timeDepths)
+                    {
+                        var runNumber = run.Item2.ToString();
+                        await CreateChannelSet($"{holeSize}in_drilling_run{runNumber.PadLeft(2, '0')} - {timeDepth} Log", holeSize, timeDepth, run);
+                    }
+                }
+            }
+        }
+
+        private async Task CreateChannelSet(string name, double holeSize, string timeDepth, Tuple<double, int, double, double> tuple)
+        {
+            ChannelSet.Uuid = DevKit.Uid();
+            ChannelSet.Citation.Title = name;
+            ChannelSet.RunNumber = tuple.Item2.ToString();
+            ChannelSet.StartIndex = new DepthIndexValue { Depth = (float)tuple.Item3 };
+            ChannelSet.EndIndex = new DepthIndexValue { Depth = (float)tuple.Item4 };
+            ChannelSet.NominalHoleSize = new LengthMeasureExt(holeSize, "in");
+            ChannelSet.TimeDepth = timeDepth;
+
+            var handler = _client.Handler<IStoreCustomer>();
+            var uri = ChannelSet.GetUri();
+
+            var dataObject = CreateDataObject(uri, ChannelSet);
+
+            // Get Object Expecting it Not to Exist
+            //await GetAndAssert(handler, uri, Energistics.EtpErrorCodes.NotFound);
+
+            // Put Object
+            await PutAndAssert(handler, dataObject);
+
+            // Get Object
+            //var args = await GetAndAssert(handler, uri);
+
+            // Check Data Object XML
+            //Assert.IsNotNull(args?.Message.DataObject);
+            //var xml = args.Message.DataObject.GetString();
+
+            //var result = Parse<ChannelSet>(xml);
+            //Assert.IsNotNull(result);
+        }
     }
 }
