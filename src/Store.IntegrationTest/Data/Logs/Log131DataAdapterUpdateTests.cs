7adf1266cb0d4c1666ac92f66f933fb69a76bbec
Joseph Puccia <joseph.puccia@pds.nl>
2017-07-11 20:44:00 -0500

    Ignored new tests until development is finished
    Related Work Items: #13680

--- src/Store.IntegrationTest/Data/Logs/Log131DataAdapterUpdateTests.cs
+++ src/Store.IntegrationTest/Data/Logs/Log131DataAdapterUpdateTests.cs
@@ -525,6 +525,7 @@ namespace PDS.WITSMLstudio.Store.Data.Logs
             DevKit.GetAndAssertDataRowCount(DevKit.CreateLog(Log), dataRowCount + totalUpdateRows);
         }
 
+        [Ignore]
         [TestMethod]
         public void Log131DataAdapter_UpdateInStore_Multi_Update_Merge_With_File_Storage()
         {
