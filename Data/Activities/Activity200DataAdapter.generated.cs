5c0d2ca9835fe7ac8e5572ddacc2e6fec8420ed9
Bobby Diaz <bobby.diaz@pds.nl>
2017-09-10 20:13:52 -0500

    Updated witsml submodule to commit [80106798]. Updated generated code for Common v2.1 decorator object queries.
    Related Work Items: #10931

--- Data/Activities/Activity200DataAdapter.generated.cs
+++ Data/Activities/Activity200DataAdapter.generated.cs
@@ -1,4 +1,4 @@
-﻿//----------------------------------------------------------------------- 
+//----------------------------------------------------------------------- 
 // PDS WITSMLstudio Store, 2017.2
 //
 // Copyright 2017 PDS Americas LLC
@@ -81,8 +81,15 @@ namespace PDS.WITSMLstudio.Store.Data.Activities
 
             if (parentUri != null)
             {
-                //var uidWellbore = parentUri.Value.ObjectId;
-                //query = query.Where(x => x.Wellbore.Uuid == uidWellbore);
+                if (!parentUri.Value.IsBaseUri && !ObjectTypes.Activity.Equals(parentUri.Value.ObjectType))
+                {
+                    var objectId = parentUri.Value.ObjectId;
+                    var contentType = parentUri.Value.ContentType.ToString();
+
+                    query = query.Where(x =>
+                        x.ActivityDescriptor.Uuid == objectId &&
+                        x.ActivityDescriptor.ContentType == contentType);
+                }
 
                 if (!string.IsNullOrWhiteSpace(parentUri.Value.Query))
                     query = query.LinqToQuerystring(parentUri.Value.Query);
