67f522d1f870a17088b9e2fb6ca3a9ec1c8dcfdb
Matthew Wooley <Matthew.Wooley@pds.nl>
2018-01-19 15:19:57 -0600

    1) Increase transaction retry attempts to 150 2) Update WITSML submodule to commit [1fc3ee00] 3) Revert changes to chunk adapter
    Related Work Items: #19253, #19446, #19476

--- src/Store.MongoDb/Properties/Settings.Designer.cs
+++ src/Store.MongoDb/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PDS.WITSMLstudio.Store.MongoDb {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -52,7 +52,7 @@ namespace PDS.WITSMLstudio.Store.MongoDb {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10")]
+        [global::System.Configuration.DefaultSettingValueAttribute("150")]
         public int DefaultMaximumTransactionAttempt {
             get {
                 return ((int)(this["DefaultMaximumTransactionAttempt"]));
