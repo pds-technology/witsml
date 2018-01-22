a00cddba5bff392c4ab754b81ec4be48c7356d59
Matthew Wooley <Matthew.Wooley@pds.nl>
2018-01-22 10:57:48 -0600

    1) Set ChannelDataChunkTimeRangeSize to 900000000 (15 minutes)
    Related Work Items: #19446

--- src/Store.Core/Properties/Settings.Designer.cs
+++ src/Store.Core/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PDS.WITSMLstudio.Store.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -106,7 +106,7 @@ namespace PDS.WITSMLstudio.Store.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5000000000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("900000000")]
         public long ChannelDataChunkTimeRangeSize {
             get {
                 return ((long)(this["ChannelDataChunkTimeRangeSize"]));
