463cff6d874094d5228056c922f508bfd28d486c
Bobby Diaz <bobby.diaz@pds.nl>
2018-04-27 20:04:19 -0500

    Added UserAdmin command line utility for adding/removing user accounts.
    Related Work Items: #26181

---
+++ src/Store.UserAdmin/GlobalAssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Store, 2018.1
+//
+// Copyright 2018 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyProduct("PDS WITSMLstudio Store")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCompany("PDS Americas LLC")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
---
+++ src/Store.UserAdmin/Program.cs
@@ -0,0 +1,115 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Store, 2018.1
+//
+// Copyright 2018 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Web.Security;
+using CommandLine;
+using PDS.WITSMLstudio.Framework.Web;
+using PDS.WITSMLstudio.Store.Security;
+
+namespace PDS.WITSMLstudio.Store.UserAdmin
+{
+    public static class Program
+    {
+        private const int Success = 0;
+        private const int Error = 1;
+
+        public static void Main(string[] args)
+        {
+            ContainerConfiguration.Register(".");
+            Console.WriteLine();
+
+            var writer = new StringWriter();
+            new Parser(with =>
+                {
+                    with.EnableDashDash = true;
+                    with.HelpWriter = writer;
+                })
+                .ParseArguments<AddOptions, RemoveOptions>(args)
+                .MapResult(
+                    (AddOptions opts) => AddUser(opts),
+                    (RemoveOptions opts) => RemoveUser(opts),
+                    errors =>
+                    {
+                        Console.WriteLine(writer);
+                        return Error;
+                    });
+        }
+
+        private static MongoDbMembershipProvider GetProvider()
+        {
+            var config = new NameValueCollection(ConfigurationManager.AppSettings);
+
+            var provider = new MongoDbMembershipProvider();
+            provider.Initialize(MongoDbMembershipProvider.ProviderName, config);
+
+            return provider;
+        }
+
+        private static int AddUser(AddOptions opts)
+        {
+            MembershipCreateStatus status;
+
+            var username = opts.Username;
+            var password = opts.Password ?? Membership.GeneratePassword(8, 2);
+            var email = opts.Email ?? "witsml@pds.nl";
+
+            var provider = GetProvider();
+            var saved = provider.GetUser(username, false);
+
+            if (saved != null)
+                return Error;
+
+            var user = provider.CreateUser(
+                username: username,
+                password: password,
+                email: email,
+                passwordQuestion: null,
+                passwordAnswer: null,
+                isApproved: true,
+                providerUserKey: null,
+                status: out status);
+
+            if (user == null || status != MembershipCreateStatus.Success)
+                return Error;
+
+            saved = provider.GetUser(username, false);
+
+            if (saved == null || !provider.ValidateUser(username, password))
+                return Error;
+
+            Console.WriteLine("email:     {0}", email);
+            Console.WriteLine("username:  {0}", username);
+            Console.WriteLine("password:  {0}", password);
+
+            return Success;
+        }
+
+        private static int RemoveUser(RemoveOptions opts)
+        {
+            var provider = GetProvider();
+
+            return provider.DeleteUser(opts.Username, true)
+                ? Success
+                : Error;
+        }
+    }
+}
