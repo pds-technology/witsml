5f3334ef5d88c754227fbca771badcee96c83c9f
Bobby Diaz <bobby.diaz@pds.nl>
2018-11-29 17:43:29 -0600

    Moving Connection related classes from Desktop.Core to Core library.
    Related Work Items: #33834

---
+++ src/Core/Connections/Connection.cs
@@ -0,0 +1,524 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Core, 2018.3
+//
+// Copyright 2018 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System.Net;
+using System.Runtime.Serialization;
+using System.Security;
+using Caliburn.Micro;
+using Energistics.Etp.Common;
+using Energistics.Etp.Common.Datatypes;
+
+namespace PDS.WITSMLstudio.Connections
+{
+    /// <summary>
+    /// Connection details for a connection
+    /// </summary>
+    [DataContract]
+    public class Connection : PropertyChangedBase
+    {
+        /// <summary>
+        /// Initializes the <see cref="Connection"/> class.
+        /// </summary>
+        static Connection()
+        {
+            AutoMapper.Mapper.Initialize(x => x.CreateMap<Connection, Connection>());
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Connection"/> class.
+        /// </summary>
+        public Connection()
+        {
+            SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            IsAuthenticationBasic = true;
+            PreAuthenticate = true;
+            ProxyPort = 80;
+            RedirectPort = 9005;
+            SubProtocol = EtpSettings.EtpSubProtocolName;
+            EtpEncoding = "binary";
+        }
+
+        private AuthenticationTypes _authenticationType;
+
+        /// <summary>
+        /// Gets or sets the authentication type.
+        /// </summary>
+        /// <value>The authentication type.</value>
+        [DataMember]
+        public AuthenticationTypes AuthenticationType
+        {
+            get { return _authenticationType; }
+            set
+            {
+                if (_authenticationType != value)
+                {
+                    _authenticationType = value;
+                    NotifyOfPropertyChange(() => AuthenticationType);
+                }
+            }
+        }
+
+        private SecurityProtocolType _securityProtocol;
+
+        /// <summary>
+        /// Gets or sets the security protocol.
+        /// </summary>
+        /// <value>The security protocol.</value>
+        [DataMember]
+        public SecurityProtocolType SecurityProtocol
+        {
+            get { return _securityProtocol; }
+            set
+            {
+                if (_securityProtocol != value)
+                {
+                    _securityProtocol = value;
+                    NotifyOfPropertyChange(() => SecurityProtocol);
+                }
+            }
+        }
+
+        private string _name;
+
+        /// <summary>
+        /// Gets or sets the name of the connection
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (!string.Equals(_name, value))
+                {
+                    _name = value;
+                    NotifyOfPropertyChange(() => Name);
+                }
+            }
+        }
+
+        private string _uri;
+
+        /// <summary>
+        /// Gets or sets the uri to access the connection
+        /// </summary>
+        [DataMember]
+        public string Uri
+        {
+            get { return _uri; }
+            set
+            {
+                if (!string.Equals(_uri, value))
+                {
+                    _uri = value;
+                    NotifyOfPropertyChange(() => Uri);
+                }
+            }
+        }
+
+        private string _clientId;
+
+        /// <summary>
+        /// Gets or sets the client ID
+        /// </summary>
+        [DataMember]
+        public string ClientId
+        {
+            get { return _clientId; }
+            set
+            {
+                if (!string.Equals(_clientId, value))
+                {
+                    _clientId = value;
+                    NotifyOfPropertyChange(() => ClientId);
+                }
+            }
+        }
+
+        private int _redirectPort;
+
+        /// <summary>
+        /// Gets or sets the redirect port.
+        /// </summary>
+        /// <value>The redirect port.</value>
+        [DataMember]
+        public int RedirectPort
+        {
+            get { return _redirectPort; }
+            set
+            {
+                if (_redirectPort != value)
+                {
+                    _redirectPort = value;
+                    NotifyOfPropertyChange(() => RedirectPort);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the proxy host name is not a URI.
+        /// </summary>
+        public bool IsProxyHostName => !ProxyHost?.Contains("://") ?? false;
+
+        private string _proxyHost;
+
+        /// <summary>
+        /// Gets or sets the proxy host name.
+        /// </summary>
+        [DataMember]
+        public string ProxyHost
+        {
+            get { return _proxyHost; }
+            set
+            {
+                if (!string.Equals(_proxyHost, value))
+                {
+                    _proxyHost = value;
+                    NotifyOfPropertyChange(() => ProxyHost);
+                    NotifyOfPropertyChange(() => IsProxyHostName);
+                }
+            }
+        }
+
+        private int _proxyPort;
+
+        /// <summary>
+        /// Gets or sets the proxy port.
+        /// </summary>
+        /// <value>The proxy port.</value>
+        [DataMember]
+        public int ProxyPort
+        {
+            get { return _proxyPort; }
+            set
+            {
+                if (_proxyPort != value)
+                {
+                    _proxyPort = value;
+                    NotifyOfPropertyChange(() => ProxyPort);
+                }
+            }
+        }
+
+        private string _proxyUsername;
+
+        /// <summary>
+        /// Gets or sets the username to authenticate with the proxy server.
+        /// </summary>
+        [DataMember]
+        public string ProxyUsername
+        {
+            get { return _proxyUsername; }
+            set
+            {
+                if (!string.Equals(_proxyUsername, value))
+                {
+                    _proxyUsername = value;
+                    NotifyOfPropertyChange(() => ProxyUsername);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the proxy password.
+        /// </summary>
+        [DataMember]
+        public string ProxyPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SecureString password to authenticate with the proxy server.
+        /// </summary>
+        public SecureString SecureProxyPassword { get; set; }
+
+        private bool _proxyUseDefaultCredentials;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use default credentials for the web proxy.
+        /// </summary>
+        [DataMember]
+        public bool ProxyUseDefaultCredentials
+        {
+            get { return _proxyUseDefaultCredentials; }
+            set
+            {
+                if (_proxyUseDefaultCredentials != value)
+                {
+                    _proxyUseDefaultCredentials = value;
+                    NotifyOfPropertyChange(() => ProxyUseDefaultCredentials);
+                }
+            }
+        }
+
+        private string _username;
+
+        /// <summary>
+        /// Gets or sets the username to authenticate the connection
+        /// </summary>
+        [DataMember]
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (!string.Equals(_username, value))
+                {
+                    _username = value;
+                    NotifyOfPropertyChange(() => Username);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        [DataMember]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SecureString password to authenticate the connection.
+        /// </summary>
+        public SecureString SecurePassword { get; set; }
+
+        private string _jsonWebToken;
+
+        /// <summary>
+        /// Gets or sets the JSON Web Token to authenticate the connection.
+        /// </summary>
+        [DataMember]
+        public string JsonWebToken
+        {
+            get { return _jsonWebToken; }
+            set
+            {
+                if (!string.Equals(_jsonWebToken, value))
+                {
+                    _jsonWebToken = value;
+                    NotifyOfPropertyChange(() => JsonWebToken);
+                }
+            }
+        }
+
+        private WebSocketType _webSocketType;
+
+        /// <summary>
+        /// Gets or sets the type of the web socket.
+        /// </summary>
+        [DataMember]
+        public WebSocketType WebSocketType
+        {
+            get { return _webSocketType; }
+            set
+            {
+                if (_webSocketType == value) return;
+                _webSocketType = value;
+                NotifyOfPropertyChange(() => WebSocketType);
+            }
+        }
+
+        private string _subProtocol;
+
+        /// <summary>
+        /// Gets or sets the sub protocol.
+        /// </summary>
+        [DataMember]
+        public string SubProtocol
+        {
+            get { return _subProtocol; }
+            set
+            {
+                if (!string.Equals(_subProtocol, value))
+                {
+                    _subProtocol = value;
+                    NotifyOfPropertyChange(() => SubProtocol);
+                }
+            }
+        }
+
+        private string _etpEncoding;
+
+        /// <summary>
+        /// Gets or sets the ETP encoding.
+        /// </summary>
+        [DataMember]
+        public string EtpEncoding
+        {
+            get { return _etpEncoding; }
+            set
+            {
+                if (!string.Equals(_etpEncoding, value))
+                {
+                    _etpEncoding = value;
+                    NotifyOfPropertyChange(() => EtpEncoding);
+                }
+            }
+        }
+
+        private string _etpCompression;
+
+        /// <summary>
+        /// Gets or sets the ETP compression.
+        /// </summary>
+        [DataMember]
+        public string EtpCompression
+        {
+            get { return _etpCompression; }
+            set
+            {
+                if (!string.Equals(_etpCompression, value))
+                {
+                    _etpCompression = value;
+                    NotifyOfPropertyChange(() => EtpCompression);
+                }
+            }
+        }
+
+        private bool _acceptInvalidCertificates;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to accept invalid certificates.
+        /// </summary>
+        [DataMember]
+        public bool AcceptInvalidCertificates
+        {
+            get { return _acceptInvalidCertificates; }
+            set
+            {
+                if (_acceptInvalidCertificates != value)
+                {
+                    _acceptInvalidCertificates = value;
+                    NotifyOfPropertyChange(() => AcceptInvalidCertificates);
+                }
+            }
+        }
+
+        private bool _preAuthenticate;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to pre-authenticate.
+        /// </summary>
+        [DataMember]
+        public bool PreAuthenticate
+        {
+            get { return _preAuthenticate; }
+            set
+            {
+                if (_preAuthenticate != value)
+                {
+                    _preAuthenticate = value;
+                    NotifyOfPropertyChange(() => PreAuthenticate);
+                }
+            }
+        }
+
+        private bool _isAuthenticationBasic;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to connect using Basic authentication.
+        /// </summary>
+        [DataMember]
+        public bool IsAuthenticationBasic
+        {
+            get { return _isAuthenticationBasic; }
+            set
+            {
+                if (_isAuthenticationBasic != value)
+                {
+                    _isAuthenticationBasic = value;
+                    IsAuthenticationBearer = !value;
+                    NotifyOfPropertyChange(() => IsAuthenticationBasic);
+                }
+            }
+        }
+        
+        private bool _isAuthenticationBearer;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to connect using Bearer authentication.
+        /// </summary>
+        [DataMember]
+        public bool IsAuthenticationBearer
+        {
+            get { return _isAuthenticationBearer; }
+            set
+            {
+                if (_isAuthenticationBearer != value)
+                {
+                    _isAuthenticationBearer = value;
+                    IsAuthenticationBasic = !value;
+                    NotifyOfPropertyChange(() => IsAuthenticationBearer);
+                }
+            }
+        }
+
+        private CompressionMethods _soapRequestCompressionMethod;
+
+        /// <summary>
+        /// Gets or sets the compression method for SOAP client requests.
+        /// </summary>
+        [DataMember]
+        public CompressionMethods SoapRequestCompressionMethod
+        {
+            get { return _soapRequestCompressionMethod; }
+            set
+            {
+                if (_soapRequestCompressionMethod != value)
+                {
+                    _soapRequestCompressionMethod = value;
+                    NotifyOfPropertyChange(() => SoapRequestCompressionMethod);
+                }
+            }
+        }
+
+        private bool _soapAcceptCompressedResponses;
+
+        /// <summary>
+        /// Gets or sets the compression method for SOAP client requests.
+        /// </summary>
+        [DataMember]
+        public bool SoapAcceptCompressedResponses
+        {
+            get { return _soapAcceptCompressedResponses; }
+            set
+            {
+                if (_soapAcceptCompressedResponses != value)
+                {
+                    _soapAcceptCompressedResponses = value;
+                    NotifyOfPropertyChange(() => SoapAcceptCompressedResponses);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"Uri: {Uri}; Username: {Username}; AuthenticationType: {AuthenticationType}; SecurityProtocol: {SecurityProtocol};" +
+                   $" ProxyHost: {ProxyHost}; ProxyPort: {ProxyPort}; ProxyUsername: {ProxyUsername}; ProxyUseDefaultCredentials: {ProxyUseDefaultCredentials};" +
+                   $" WebSocketType: {WebSocketType};" +
+                   (!string.IsNullOrWhiteSpace(SubProtocol) ? $" SubProtocol: {SubProtocol};" : string.Empty) +
+                   (!string.IsNullOrWhiteSpace(EtpEncoding) ? $" EtpEncoding: {EtpEncoding};" : string.Empty) +
+                   (!string.IsNullOrWhiteSpace(EtpCompression) ? $" EtpCompression: {EtpCompression};" : string.Empty) +
+                   $" SoapRequestCompressionMethod: {SoapRequestCompressionMethod};" +
+                   $" SoapAcceptCompressedResponses: {SoapAcceptCompressedResponses};";
+        }
+    }
+}
---
+++ src/Core/Connections/ConnectionExtensions.cs
@@ -0,0 +1,219 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Core, 2018.3
+//
+// Copyright 2018 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Energistics.DataAccess;
+using System.Net;
+using Energistics.Etp;
+using Energistics.Etp.Common;
+
+namespace PDS.WITSMLstudio.Connections
+{
+    /// <summary>
+    /// Defines static helper methods that can be used to configure WITSML store connections.
+    /// </summary>
+    public static class ConnectionExtensions
+    {
+        /// <summary>
+        /// Creates a WITSMLWebServiceConnection for the current connection uri and WITSML version.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <param name="version">The WITSML version.</param>
+        /// <returns>A <see cref="WITSMLWebServiceConnection"/> instance.</returns>
+        public static WITSMLWebServiceConnection CreateProxy(this Connection connection, WMLSVersion version)
+        {
+            //_log.DebugFormat("A new Proxy is being created with URI: {0}; WitsmlVersion: {1};", connection.Uri, version);
+            return connection.UpdateProxy(new WITSMLWebServiceConnection(connection.Uri, version));
+        }
+
+        /// <summary>
+        /// Updates a WITSMLWebServiceConnection for the current connection settings.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <param name="proxy">The WITSML web service proxy.</param>
+        /// <returns>The <see cref="WITSMLWebServiceConnection"/> instance.</returns>
+        public static WITSMLWebServiceConnection UpdateProxy(this Connection connection, WITSMLWebServiceConnection proxy)
+        {
+            proxy.Proxy = connection.CreateWebProxy();
+            proxy.Url = connection.Uri;
+            proxy.Timeout *= 5;
+            proxy.AcceptCompressedResponses = connection.SoapAcceptCompressedResponses;
+            proxy.CompressRequests = connection.SoapRequestCompressionMethod == CompressionMethods.Gzip;
+
+            connection.SetServerCertificateValidation();
+
+            if (!string.IsNullOrWhiteSpace(connection.Username))
+            {
+                if (connection.PreAuthenticate)
+                {
+                    proxy.Headers = connection.GetAuthorizationHeader();
+                    proxy.IsPreAuthenticationEnabled = connection.PreAuthenticate;
+                }
+                else
+                {
+                    proxy.Username = connection.Username;
+                    proxy.SetSecurePassword(connection.SecurePassword);
+                }
+            }
+
+            return proxy;
+        }
+
+        /// <summary>
+        /// Creates a web proxy for the current connection settings.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>A new <see cref="WebProxy"/> instance.</returns>
+        public static WebProxy CreateWebProxy(this Connection connection)
+        {
+            if (string.IsNullOrWhiteSpace(connection.ProxyHost)) return null;
+
+            var proxy = connection.ProxyHost.Contains("://")
+                ? new WebProxy(new Uri(connection.ProxyHost))
+                : new WebProxy(connection.ProxyHost, connection.ProxyPort);
+
+            if (!string.IsNullOrWhiteSpace(connection.ProxyUsername) &&
+                !string.IsNullOrWhiteSpace(connection.ProxyPassword))
+            {
+                proxy.Credentials = new NetworkCredential(connection.ProxyUsername, connection.SecureProxyPassword);
+            }
+
+            proxy.UseDefaultCredentials = connection.ProxyUseDefaultCredentials;
+
+            return proxy;
+        }
+
+        /// <summary>
+        /// Creates a Json client for the current connection uri.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>An <see cref="Energistics.Etp.JsonClient"/> instance.</returns>
+        public static JsonClient CreateJsonClient(this Connection connection)
+        {
+            return connection.IsAuthenticationBasic
+                ? new JsonClient(connection.Username, connection.Password, connection.CreateWebProxy())
+                : new JsonClient(connection.JsonWebToken, connection.CreateWebProxy());
+        }
+
+        /// <summary>
+        /// Creates an ETP client for the current connection
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <param name="applicationName">Name of the application.</param>
+        /// <param name="applicationVersion">The application version.</param>
+        /// <returns>An <see cref="Energistics.Etp.Common.IEtpClient"/> instance.</returns>
+        public static IEtpClient CreateEtpClient(this Connection connection, string applicationName, string applicationVersion)
+        {
+            var headers = connection.GetAuthorizationHeader();
+
+            connection.UpdateEtpSettings(headers);
+            connection.SetServerCertificateValidation();
+
+            var client = EtpFactory.CreateClient(connection.WebSocketType, connection.Uri, applicationName, applicationVersion, connection.SubProtocol, headers);
+
+            if (!string.IsNullOrWhiteSpace(connection.ProxyHost))
+            {
+                client.SetProxy(connection.ProxyHost, connection.ProxyPort,
+                                connection.ProxyUsername, connection.ProxyPassword);
+            }
+            if (!string.IsNullOrWhiteSpace(connection.EtpCompression))
+            {
+                client.SetSupportedCompression(connection.EtpCompression);
+            }
+
+            return client;
+        }
+
+        /// <summary>
+        /// Gets the authorization header for the current connection.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>The authorization header, as a dictionary.</returns>
+        public static IDictionary<string, string> GetAuthorizationHeader(this Connection connection)
+        {
+            return connection.IsAuthenticationBasic
+                   ? Energistics.Etp.Security.Authorization.Basic(connection.Username, connection.Password)
+                   : Energistics.Etp.Security.Authorization.Bearer(connection.JsonWebToken);
+        }
+
+        /// <summary>
+        /// Sets the server certificate validation.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        public static void SetServerCertificateValidation(this Connection connection)
+        {
+            ServicePointManager.SecurityProtocol = connection.SecurityProtocol;
+
+            if (connection.AcceptInvalidCertificates)
+                ServicePointManager.ServerCertificateValidationCallback +=
+                    (sender, certificate, chain, sslPolicyErrors) => true;
+            else
+                ServicePointManager.ServerCertificateValidationCallback = null;
+        }
+
+        /// <summary>
+        /// Gets the ETP server capabilities URL for the connection.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <param name="includeGetVersion">if set to <c>true</c> include the GetVersion query parameter.</param>
+        /// <returns>The well-known server capabilities URL.</returns>
+        public static string GetEtpServerCapabilitiesUrl(this Connection connection, bool includeGetVersion = true)
+        {
+            if (string.IsNullOrWhiteSpace(connection?.Uri))
+                return string.Empty;
+
+            var etpVersion = includeGetVersion
+                ? $"?{EtpSettings.GetVersionHeader}={WebUtility.UrlEncode(connection.SubProtocol)}"
+                : string.Empty;
+
+            return $"http{connection.Uri.Substring(2)}/.well-known/etp-server-capabilities{etpVersion}";
+        }
+
+        /// <summary>
+        /// Gets the ETP server capabilities for the connection.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>The server capabilities result</returns>
+        public static object GetEtpServerCapabilities(this Connection connection) =>
+            CreateJsonClient(connection).GetServerCapabilities(GetEtpServerCapabilitiesUrl(connection));
+
+        /// <summary>
+        /// Gets the ETP versions for the connection.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>The ETP versions</returns>
+        public static IList<string> GetEtpVersions(this Connection connection)
+        {
+            var url = $"{GetEtpServerCapabilitiesUrl(connection, false)}?{EtpSettings.GetVersionsHeader}=true";
+            return CreateJsonClient(connection).GetEtpVersions(url);
+        }
+
+        /// <summary>
+        /// Updates the ETP settings based on the connection settings.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <param name="headers">The headers.</param>
+        public static void UpdateEtpSettings(this Connection connection, IDictionary<string, string> headers)
+        {
+            // Allow settings to be blanked out via Connection dialog
+            EtpSettings.EtpSubProtocolName = connection.SubProtocol ?? string.Empty;
+            headers[EtpSettings.EtpEncodingHeader] = connection.EtpEncoding ?? string.Empty;
+        }
+    }
+}
