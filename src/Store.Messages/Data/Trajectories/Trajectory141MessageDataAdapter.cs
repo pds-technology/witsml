c348e2625187eb97a43bfb37f6f2ec063ea2a300
Georgi Panayotov <georgi.panayotov@pds.nl>
2019-11-01 09:54:11 +0000

    Merged PR 2069: Handle trajectories in Store.Messages
    We need to handle trajectories in Dr.Q and I've noticed that these are not currently handled in Store.Messages. Seems like quite a straightforward extension (I've copied over from Well), but I was wondering if that is going to affect any other clients of the Store.Messages API.
    
    Additionally I've updated the Kafka libraries to their latest stable versions so that they are matching what we use in Dr.Q internally.
    
    Related work items: #46754

---
+++ src/Store.Messages/Data/Trajectories/Trajectory141MessageDataAdapter.cs
@@ -0,0 +1,63 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Core, 2018.3
+//
+// Copyright 2018 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using Energistics.DataAccess.WITSML141;
+using PDS.WITSMLstudio.Framework;
+using PDS.WITSMLstudio.Store.Configuration;
+
+namespace PDS.WITSMLstudio.Store.Data.Trajectories
+{
+    /// <summary>
+    /// Data adapter that encapsulates CRUD functionality for <see cref="Trajectory" />
+    /// </summary>
+    /// <seealso cref="PDS.WITSMLstudio.Store.Data.MessageDataAdapter{Trajectory}" />
+    /// <seealso cref="PDS.WITSMLstudio.Store.Configuration.IWitsml141Configuration" />
+    [Export(typeof(IWitsml141Configuration))]
+    [Export(typeof(IWitsmlDataAdapter<Trajectory>))]
+    [Export141(ObjectTypes.Trajectory, typeof(IWitsmlDataAdapter))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class Trajectory141MessageDataAdapter : MessageDataAdapter<Trajectory>, IWitsml141Configuration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Trajectory141MessageDataAdapter"/> class.
+        /// </summary>
+        /// <param name="container">The composition container.</param>
+        [ImportingConstructor]
+        public Trajectory141MessageDataAdapter(IContainer container) : base(container, ObjectNames.Trajectory141)
+        {
+            Logger.Verbose("Instance created.");
+        }
+
+        /// <summary>
+        /// Gets the server capabilities.
+        /// </summary>
+        /// <param name="capServer">The capServer instance.</param>
+        public void GetCapabilities(CapServer capServer)
+        {
+            Logger.DebugFormat("Getting the supported capabilities for Trajectory data version {0}.", capServer.Version);
+
+            if (MessageHandler.IsQueryEnabled)
+                capServer.Add(Functions.GetFromStore, ObjectTypes.Trajectory);
+
+            capServer.Add(Functions.AddToStore, ObjectTypes.Trajectory);
+            capServer.Add(Functions.UpdateInStore, ObjectTypes.Trajectory);
+            //capServer.Add(Functions.DeleteFromStore, ObjectTypes.Well);
+        }
+    }
+}
