2869652fa209d12d8fc3ba8f2bd22c617385c9ed
Eric Griffith <eric.griffith@pds.nl>
2020-03-26 17:58:34 -0500

    Merging witsml-server into witsml.
    Related Work Items: #50227

--- src/Store/Properties/AssemblyInfo.cs
+++ src/Store/Properties/AssemblyInfo.cs
@@ -22,6 +22,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyProduct("PDS WITSMLstudio Store")]
 [assembly: AssemblyTitle("PDS WITSMLstudio Store")]
 [assembly: AssemblyDescription("PDS WITSMLstudio Store Web Application")]
 
